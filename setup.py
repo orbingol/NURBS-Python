acc1a8fa8391a32b0ddcd476c48389a8506e5f5b
Onur Rauf Bingol <orbingol@gmail.com>
2017-03-02 19:58:00 -0600

    Minor reformmating of setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from distutils.core import setup
 import os
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-	name='NURBS-Python',
-	version='2.0-dev',
-	description='NURBS curve and surface calculation algorithms in native Python',
-	author='Onur Rauf Bingol',
-	license='MIT',
-	url='https://github.com/orbingol/NURBS-Python',
-	packages=['nurbs'],
-	long_description=read('README.md'),
-	classifiers=[
-	'License :: OSI Approved :: MIT License',
-	'Topic :: Scientific/Engineering :: Mathematics'
-	]
-	)
+    name='NURBS-Python',
+    version='2.0',
+    description='NURBS curve and surface calculation algorithms in native Python',
+    author='Onur Rauf Bingol',
+    license='MIT',
+    url='https://github.com/orbingol/NURBS-Python',
+    packages=['nurbs'],
+    long_description=read('README.md'),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ]
+)
