3f15c6b8f9fb72a34083c811046da4358f56a99b
Onur Rauf Bingol <orbingol@gmail.com>
2017-03-07 23:26:06 -0600

    Updated surface example 2

--- ex_surface02.py
+++ ex_surface02.py
@@ -15,19 +15,19 @@ surf.degree_v = 3
 surf.knotvector_u = utils.autogen_knotvector(surf.degree_u, 6)
 surf.knotvector_v = utils.autogen_knotvector(surf.degree_v, 6)
 
-# Calculate surface points
-surf.evaluate()
+# Evaluate surface
+surf.evaluate_rational()
 
 # Calculate 1st order surface derivative at the given u and v
 u = 0.2
 v = 0.9
-skl = surf.derivatives(u, v, 1)
-print("* Surface point at u = %.2f and v = %.2f is (%.2f, %.2f, %.2f)" % (u, v, skl[0][0][0], skl[0][0][1], skl[0][0][2]))
-print("* First derivative w.r.t. u is (%.2f, %.2f, %.2f)" % (skl[1][0][0], skl[1][0][1], skl[1][0][2]))
-print("* First derivative w.r.t. v is (%.2f, %.2f, %.2f)\n" % (skl[0][1][0], skl[0][1][1], skl[0][1][2]))
+surftan = surf.tangent(u, v)
+print("* Surface point at u = %.2f and v = %.2f is (%.2f, %.2f, %.2f)" % (u, v, surftan[0][0], surftan[0][1], surftan[0][2]))
+print("* First derivative w.r.t. u is (%.2f, %.2f, %.2f)" % (surftan[1][0], surftan[1][1], surftan[1][2]))
+print("* First derivative w.r.t. v is (%.2f, %.2f, %.2f)\n" % (surftan[2][0], surftan[2][1], surftan[2][2]))
 # Calculate normal at the given u and v
 norm = surf.normal(u, v)
-print("* Normal at u = %.2f and v = %.f is [%.1f, %.1f, %.1f]\n" % (u, v, norm[0], norm[1], norm[2]))
+print("* Normal at u = %.2f and v = %.2f is [%.1f, %.1f, %.1f]\n" % (u, v, norm[0], norm[1], norm[2]))
 
 # Arrange calculated surface data for plotting
 surfpts_x = []
