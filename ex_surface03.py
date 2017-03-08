cdf32509a8ad1b352f1947bd13cb91a4142455ee
Onur Rauf Bingol <orbingol@gmail.com>
2017-03-08 16:36:00 -0600

    Minor code reformatting

--- ex_surface03.py
+++ ex_surface03.py
@@ -9,12 +9,12 @@ from mpl_toolkits.mplot3d import Axes3D
 surf = ns.Surface()
 
 # Set up the NURBS surface
-surf.read_ctrlptsw("data/CP_Surface3.txt")
+surf.read_ctrlptsw("data/CPw_Surface3.txt")
 surf.degree_u = 2
 surf.degree_v = 1
 
-surf.knotvector_u = [0,0,0,0.25,0.25,0.5,0.5,0.75,0.75,1,1,1]
-surf.knotvector_v = [0,0,1,1]
+surf.knotvector_u = [0, 0, 0, 0.25, 0.25, 0.5, 0.5, 0.75, 0.75, 1, 1, 1]
+surf.knotvector_v = [0, 0, 1, 1]
 
 # Evaluate surface
 surf.evaluate_rational()
@@ -31,8 +31,7 @@ for spt in surf.surfpts:
 # Plot using Matplotlib
 fig = plt.figure(figsize=(10.67, 8), dpi=96)
 ax = fig.gca(projection='3d')
-surfplt = ax.scatter(surfpts_x, surfpts_y, surfpts_z, c="green", s=10, depthshade=True) # 3D Scatter plot
-#surfplt = ax.plot_trisurf(surfpts_x, surfpts_y, surfpts_z, cmap=plt.cm.winter)  # 3D Tri-Surface plot
+surfplt = ax.scatter(surfpts_x, surfpts_y, surfpts_z, c="green", s=10, depthshade=True)  # 3D Scatter plot
 ax.set_xlim(-2, 2)
 ax.set_ylim(-2, 2)
 ax.set_zlim(-1, 1)
