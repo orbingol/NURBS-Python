73ed24a470dfe13789f4aa3d293539324fae9f1f
Onur R. Bingol <orbingol@gmail.com>
2017-11-02 13:46:13 -0500

    Update sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -30,12 +30,16 @@ sys.path.insert(0, os.path.abspath('../'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.doctest',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.imgmath',
+              'sphinx.ext.ifconfig',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.autosummary']
+
+autodoc_member_order = 'bysource'
+autosummary_generate = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
