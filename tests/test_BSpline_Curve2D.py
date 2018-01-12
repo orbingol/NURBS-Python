eac5fe33d2453ec46e895fad9050b0083fe37a65
Onur R. Bingol <orbingol@gmail.com>
2018-01-12 15:52:45 -0600

    Add BSpline.Curve2D evaluation tests

---
+++ tests/test_BSpline_Curve2D.py
@@ -0,0 +1,120 @@
+from geomdl import BSpline
+
+GEOMDL_DELTA = 0.001
+
+
+def test_curve2d_eval1():
+
+    # Create a BSpline (NUBS) curve instance
+    curve = BSpline.Curve2D()
+
+    # Set curve degree
+    curve.degree = 3
+
+    # Set control points
+    curve.ctrlpts = [[5.0, 5.0], [10.0, 10.0], [20.0, 15.0], [35.0, 15.0], [45.0, 10.0], [50.0, 5.0]]
+
+    # Set knot vector
+    curve.knotvector = [0.0, 0.0, 0.0, 0.0, 0.33, 0.66, 1.0, 1.0, 1.0, 1.0]
+
+    # Evaluate curve
+    evalpt = curve.curvept(0.0)
+
+    # Evaluation result
+    res = [5.0, 5.0]
+
+    assert abs(evalpt[0] - res[0]) < GEOMDL_DELTA
+    assert abs(evalpt[1] - res[1]) < GEOMDL_DELTA
+
+
+def test_curve2d_eval2():
+
+    # Create a BSpline (NUBS) curve instance
+    curve = BSpline.Curve2D()
+
+    # Set curve degree
+    curve.degree = 3
+
+    # Set control points
+    curve.ctrlpts = [[5.0, 5.0], [10.0, 10.0], [20.0, 15.0], [35.0, 15.0], [45.0, 10.0], [50.0, 5.0]]
+
+    # Set knot vector
+    curve.knotvector = [0.0, 0.0, 0.0, 0.0, 0.33, 0.66, 1.0, 1.0, 1.0, 1.0]
+
+    # Evaluate curve
+    evalpt = curve.curvept(0.3)
+
+    # Evaluation result
+    res = [18.617, 13.377]
+
+    assert abs(evalpt[0] - res[0]) < GEOMDL_DELTA
+    assert abs(evalpt[1] - res[1]) < GEOMDL_DELTA
+
+
+def test_curve2d_eval3():
+    # Create a BSpline (NUBS) curve instance
+    curve = BSpline.Curve2D()
+
+    # Set curve degree
+    curve.degree = 3
+
+    # Set control points
+    curve.ctrlpts = [[5.0, 5.0], [10.0, 10.0], [20.0, 15.0], [35.0, 15.0], [45.0, 10.0], [50.0, 5.0]]
+
+    # Set knot vector
+    curve.knotvector = [0.0, 0.0, 0.0, 0.0, 0.33, 0.66, 1.0, 1.0, 1.0, 1.0]
+
+    # Evaluate curve
+    evalpt = curve.curvept(0.5)
+
+    # Evaluation result
+    res = [27.645, 14.691]
+
+    assert abs(evalpt[0] - res[0]) < GEOMDL_DELTA
+    assert abs(evalpt[1] - res[1]) < GEOMDL_DELTA
+
+
+def test_curve2d_eval4():
+    # Create a BSpline (NUBS) curve instance
+    curve = BSpline.Curve2D()
+
+    # Set curve degree
+    curve.degree = 3
+
+    # Set control points
+    curve.ctrlpts = [[5.0, 5.0], [10.0, 10.0], [20.0, 15.0], [35.0, 15.0], [45.0, 10.0], [50.0, 5.0]]
+
+    # Set knot vector
+    curve.knotvector = [0.0, 0.0, 0.0, 0.0, 0.33, 0.66, 1.0, 1.0, 1.0, 1.0]
+
+    # Evaluate curve
+    evalpt = curve.curvept(0.6)
+
+    # Evaluation result
+    res = [32.143, 14.328]
+
+    assert abs(evalpt[0] - res[0]) < GEOMDL_DELTA
+    assert abs(evalpt[1] - res[1]) < GEOMDL_DELTA
+
+
+def test_curve2d_eval5():
+    # Create a BSpline (NUBS) curve instance
+    curve = BSpline.Curve2D()
+
+    # Set curve degree
+    curve.degree = 3
+
+    # Set control points
+    curve.ctrlpts = [[5.0, 5.0], [10.0, 10.0], [20.0, 15.0], [35.0, 15.0], [45.0, 10.0], [50.0, 5.0]]
+
+    # Set knot vector
+    curve.knotvector = [0.0, 0.0, 0.0, 0.0, 0.33, 0.66, 1.0, 1.0, 1.0, 1.0]
+
+    # Evaluate curve
+    evalpt = curve.curvept(1.0)
+
+    # Evaluation result
+    res = [50.0, 5.0]
+
+    assert abs(evalpt[0] - res[0]) < GEOMDL_DELTA
+    assert abs(evalpt[1] - res[1]) < GEOMDL_DELTA
