aed288dbe85a8d4f073177fa41e23ebdcb087420
Onur R. Bingol <orbingol@gmail.com>
2018-01-30 22:35:57 -0600

    Add meta data to __init__.py

--- geomdl/__init__.py
+++ geomdl/__init__.py
@@ -1,7 +1,14 @@
-""" NURBS-Python Main Component
+""" NURBS-Python
+
+This project aims to implement Non-Uniform Rational B-Spline (NURBS) curve and surface computation algorithms in native
+Python with minimum possible dependencies. The library is fully object-oriented and does not depend on any external
+C/C++ libraries.
 
 .. moduleauthor:: Onur Rauf Bingol <orbingol@gmail.com>
 
 """
 
+__doc__ = "NURBS 2D/3D curve and surface evaluation library in native Python"
+__author__ = "Onur Rauf Bingol"
 __version__ = "3.0.19"
+__license__ = "MIT"
