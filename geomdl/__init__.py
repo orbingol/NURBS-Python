d65a4a014d60e85c5d50cad5916d6205edb1db32
Onur R. Bingol <orbingol@gmail.com>
2018-01-31 11:38:40 -0600

    Fix __doc__

--- geomdl/__init__.py
+++ geomdl/__init__.py
@@ -1,14 +1,9 @@
-""" NURBS-Python
-
-This project aims to implement Non-Uniform Rational B-Spline (NURBS) curve and surface computation algorithms in native
-Python with minimum possible dependencies. The library is fully object-oriented and does not depend on any external
-C/C++ libraries.
+""" NURBS & B-Spline 2D/3D curve and surface evaluation library in pure Python
 
 .. moduleauthor:: Onur Rauf Bingol <orbingol@gmail.com>
 
 """
 
-__doc__ = "NURBS 2D/3D curve and surface evaluation library in native Python"
 __author__ = "Onur Rauf Bingol"
 __version__ = "3.0.20"
 __license__ = "MIT"
