f40b3cf46e26686f93574e9d616b920e51e255ed
Onur R. Bingol <orbingol@gmail.com>
2018-02-10 18:48:31 -0600

    Add convert3d method

--- geomdl/NURBS.py
+++ geomdl/NURBS.py
@@ -283,6 +283,27 @@ class Curve2D(Curve):
         """
         return "coord x, coord y, scalar\n"
 
+    def convert3d(self):
+        """ Converts 2D curve to a 3D curve.
+
+        :return: 3D curve
+        :rtype: NURBS.Curve
+        """
+        # Modify weighted control points
+        new_ctrlpts = []
+        for point in self._control_points:
+            temp = [point[0], point[1], 0.0, point[2]]
+            new_ctrlpts.append(temp)
+
+        # Convert to 3D curve
+        ret_val = Curve()
+        ret_val.degree = self.degree
+        ret_val.ctrlpts = new_ctrlpts
+        ret_val.knotvector = self.knotvector
+        ret_val.delta = self.delta
+
+        return ret_val
+
 
 class Surface(BSpline.Surface):
     """ Data storage and evaluation class for NURBS surfaces.
