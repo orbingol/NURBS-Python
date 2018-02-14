aa6d96130c17efcc16568e91cc8d20cc2f939d53
Onur R. Bingol <orbingol@gmail.com>
2018-02-14 11:33:55 -0600

    Update cylinder inline documentation

--- geomdl/shapes/surface.py
+++ geomdl/shapes/surface.py
@@ -13,7 +13,7 @@ from geomdl import NURBS
 def cylinder(radius=1, height=1):
     """ Generates a NURBS cylindrical surface.
 
-    This example is kindly provided by John-Eric Dufour.
+    The cylindrical surface example is kindly contributed by John-Eric Dufour.
 
     :param radius: radius of the cylinder
     :type radius: int, float
