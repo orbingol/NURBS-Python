c90f533c30633cf13a4fe40fe39673b3866e78a0
Onur R. Bingol <orbingol@gmail.com>
2018-03-13 01:59:32 -0500

    Minor rearrangement in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import os
 import re
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 
 # Implemented from http://stackoverflow.com/a/41110107
@@ -16,11 +16,10 @@ def get_property(prop, project):
     return result.group(1)
 
 
-main_module = 'geomdl'
 setup(
     name='NURBS-Python',
-    version=get_property('__version__', main_module),
-    description='NURBS curve and surface evaluation library in pure Python',
+    version=get_property('__version__', 'geomdl'),
+    description='NURBS curve and surface evaluation library in pure python',
     author='Onur Rauf Bingol',
     author_email='contact@onurbingol.net',
     license='MIT',
@@ -33,6 +32,13 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Visualization'
-    ]
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ],
+    project_urls={
+        'Documentation': 'http://nurbs-python.rtfd.org/',
+        'Source': 'https://github.com/orbingol/NURBS-Python',
+        'Tracker': 'https://github.com/orbingol/NURBS-Python/issues',
+    },
 )
