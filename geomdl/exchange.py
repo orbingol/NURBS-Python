14f79a0913e67d6f563be99deea7e7708bf6b409
Onur R. Bingol <orbingol@gmail.com>
2018-03-28 17:52:31 -0500

    Added save_off method

--- geomdl/exchange.py
+++ geomdl/exchange.py
@@ -369,3 +369,140 @@ def save_stl_binary_multi(surface_list=(), file_name=None, vertex_spacing=2):
                 fp.write(b'\0\0')  # attribute byte count
     except IOError:
         print("Cannot open " + str(file_name) + " for writing")
+
+
+def save_off(surf_in=None, file_name=None, vertex_spacing=2):
+    """ Exports surface(s) as a .off file.
+
+    This function works as a router function between possible exporting options.
+
+    :param surf_in: surface or surfaces to be saved
+    :type surf_in: Abstract.Surface or Multi.MultiSurface
+    :param file_name: name of the output file
+    :type file_name: str
+    :param vertex_spacing: size of the triangle edge in terms of points sampled on the surface
+    :type vertex_spacing: int
+    """
+    if isinstance(surf_in, Multi.MultiSurface):
+        save_off_multi(surf_in, file_name, vertex_spacing)
+    else:
+        save_off_single(surf_in, file_name, vertex_spacing)
+
+
+# Saves a B-Spline and/or NURBS surface in Object File Format (OFF)
+def save_off_single(surface=None, file_name=None, vertex_spacing=2):
+    """ Saves a single surface as a .off file.
+
+    :param surface: surface to be saved
+    :type surface: Abstract.Surface
+    :param file_name: name of the output file
+    :type file_name: str
+    :param vertex_spacing: size of the triangle edge in terms of points sampled on the surface
+    :type vertex_spacing: int
+    """
+    # Input validity checking
+    if not isinstance(surface, Abstract.Surface):
+        raise ValueError("Input is not a surface")
+    if not file_name:
+        raise ValueError("File name field is required")
+    if vertex_spacing < 1 or not isinstance(vertex_spacing, int):
+        raise ValueError("Vertex spacing must be an integer value and it must be bigger than zero")
+
+    # Create the file and start saving triangulated surface points
+    try:
+        with open(file_name, 'w') as fp:
+            fp.write("OFF\n")
+            vertices, triangles = exh.make_obj_triangles(surface.surfpts,
+                                                         int((1.0 / surface.delta) + 1),
+                                                         int((1.0 / surface.delta) + 1),
+                                                         vertex_spacing)
+
+            line = str(len(vertices) * len(vertices[0])) + " " + str(len(triangles)) + " 0\n"
+            fp.write(line)
+            # Write vertices
+            for vert_row in vertices:
+                for vert in vert_row:
+                    line = str(vert.x) + " " + str(vert.y) + " " + str(vert.z) + "\n"
+                    fp.write(line)
+
+            # Write faces (zero-indexed)
+            for t in triangles:
+                vl = t.vertex_ids
+                line = "3 " + str(vl[0] - 1) + " " + str(vl[1] - 1) + " " + str(vl[2] - 1) + "\n"
+                fp.write(line)
+    except IOError:
+        print("Cannot open " + str(file_name) + " for writing")
+
+
+# Saves B-Spline and/or NURBS surfaces in Object File Format (OFF)
+def save_off_multi(surface_list=(), file_name=None, vertex_spacing=2):
+    """ Saves multiple surfaces as a single .off file.
+
+    :param surface_list: list of surfaces to be saved
+    :type surface_list: Multi.MultiSurface
+    :param file_name: name of the output file
+    :type file_name: str
+    :param vertex_spacing: size of the triangle edge in terms of points sampled on the surface
+    :type vertex_spacing: int
+    """
+    # Input validity checking
+    if not isinstance(surface_list, Multi.MultiSurface):
+        raise ValueError("Input must be a list of surfaces")
+    if not file_name:
+        raise ValueError("File name field is required")
+    if vertex_spacing < 1 or not isinstance(vertex_spacing, int):
+        raise ValueError("Vertex spacing must be an integer value and it must be bigger than zero")
+
+    # Create the file and start saving triangulated surface points
+    try:
+        with open(file_name, 'w') as fp:
+            vertex_offset = 0  # count the vertices to update the face numbers correctly
+
+            # Initialize lists for vertices, vertex normals and faces
+            str_v = []
+            str_f = []
+
+            # Loop through MultiSurface object
+            for surface in surface_list:
+                if not isinstance(surface, Abstract.Surface):
+                    warnings.warn("Encountered a non-surface object")
+                    continue
+
+                # Set surface delta
+                surface.delta = surface_list.delta
+
+                # Generate triangles
+                vertices, triangles = exh.make_obj_triangles(surface.surfpts,
+                                                             int((1.0 / surface.delta) + 1),
+                                                             int((1.0 / surface.delta) + 1),
+                                                             vertex_spacing)
+
+                # Collect vertices
+                for vert_row in vertices:
+                    for vert in vert_row:
+                        line = str(vert.x) + " " + str(vert.y) + " " + str(vert.z) + "\n"
+                        str_v.append(line)
+
+                # Collect faces (zero0indexed)
+                for t in triangles:
+                    vl = t.vertex_ids
+                    line = "3 " + \
+                           str(vl[0] - 1 + vertex_offset) + " " + \
+                           str(vl[1] - 1 + vertex_offset) + " " + \
+                           str(vl[2] - 1 + vertex_offset) + "\n"
+                    str_f.append(line)
+
+                # Update vertex offset
+                vertex_offset = len(str_v)
+
+            # Write file header
+            fp.write("OFF\n")
+            fp.write(str(len(str_v)) + " " + str(len(str_f)) + " 0\n")
+
+            # Write all collected data to the file
+            for line in str_v:
+                fp.write(line)
+            for line in str_f:
+                fp.write(line)
+    except IOError:
+        print("Cannot open " + str(file_name) + " for writing")
