a3af96e47f620cd08fbd837f21b0397eee279e36
Onur R. Bingol <orbingol@gmail.com>
2018-05-01 11:41:33 -0500

    Issue #25 add a test for surface pickled load and save

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -15,6 +15,14 @@ C_DEGREE = 2
 C_CTRLPTS3D = [[1, 1, 0], [2, 1, -1], [2, 2, 0]]
 C_KV = [0, 0, 0, 1, 1, 1]
 
+S_DEGREE_U = 2
+S_DEGREE_V = 2
+S_CTRLPTS = [[0, 0, 0], [0, 1, 0], [0, 2, -3],
+             [1, 0, 6], [1, 1, 0], [1, 2, 0],
+             [2, 0, 0], [2, 1, 0], [2, 2, 3]]
+S_KV_U = [0, 0, 0, 1, 1, 1]
+S_KV_V = [0, 0, 0, 1, 1, 1]
+
 
 def test_bspline_curve_loadsave():
     curve_save = BSpline.Curve()
@@ -33,3 +41,30 @@ def test_bspline_curve_loadsave():
     assert curve_save.knotvector == curve_load.knotvector
     assert curve_save.ctrlpts == curve_load.ctrlpts
     assert curve_save.dimension == curve_load.dimension
+
+
+def test_bspline_surface_loadsave():
+    surf_save = BSpline.Surface()
+    surf_save.degree_u = S_DEGREE_U
+    surf_save.degree_v = S_DEGREE_V
+    surf_save.ctrlpts_size_u = 3
+    surf_save.ctrlpts_size_v = 3
+    surf_save.ctrlpts = S_CTRLPTS
+    surf_save.knotvector_u = S_KV_U
+    surf_save.knotvector_v = S_KV_V
+    surf_save.save(FILE_NAME)
+
+    surf_load = BSpline.Surface()
+    surf_load.load(FILE_NAME)
+
+    # Remove save file
+    os.remove(FILE_NAME)
+
+    assert surf_save.degree_u == surf_load.degree_u
+    assert surf_save.degree_v == surf_load.degree_v
+    assert surf_save.knotvector_u == surf_load.knotvector_u
+    assert surf_save.knotvector_v == surf_load.knotvector_v
+    assert surf_save.ctrlpts == surf_load.ctrlpts
+    assert surf_save.ctrlpts_size_u == surf_load.ctrlpts_size_u
+    assert surf_save.ctrlpts_size_v == surf_load.ctrlpts_size_v
+    assert surf_save.dimension == surf_load.dimension
