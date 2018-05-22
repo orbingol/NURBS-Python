4a56523482f567abac9f6c4863dd980204f82c8c
Onur R. Bingol <orbingol@gmail.com>
2018-05-22 08:14:55 +0300

    Add surface visualization module for VisPlotly

--- geomdl/visualization/VisPlotly.py
+++ geomdl/visualization/VisPlotly.py
@@ -8,6 +8,7 @@
 """
 
 from . import Abstract
+from . import utilities
 
 from . import numpy as np
 import plotly
@@ -116,6 +117,69 @@ class VisCurve3D(Abstract.VisAbstract):
 
             # Plot evaluated points
             if plot['type'] == 'evalpts':
+                figure = graph_objs.Scatter3d(
+                    x=pts[:, 0],
+                    y=pts[:, 1],
+                    z=pts[:, 2],
+                    name=plot['name'],
+                    mode='lines',
+                    line=dict(
+                        color=plot['color'],
+                        width=self._config.line_width
+                    ),
+                )
+                plot_data.append(figure)
+
+        plot_layout = dict(
+            width=self._config.figure_size[0],
+            height=self._config.figure_size[1],
+            autosize=False,
+            showlegend=self._config.display_legend
+        )
+
+        plotly.offline.plot({
+            "data": plot_data,
+            "layout": plot_layout
+        }, show_link=False)
+
+
+class VisSurface(Abstract.VisAbstractSurf):
+    """ Plotly visualization module for Surfaces """
+    def __init__(self, config=VisConfig()):
+        super(VisSurface, self).__init__(config=config)
+
+    def render(self):
+        """ Plots the surface and the control points grid """
+        if not self._plots:
+            return
+
+        plot_data = []
+        for plot in self._plots:
+            # Plot control points
+            if plot['type'] == 'ctrlpts' and self._config.display_ctrlpts:
+                pts = np.array(utilities.make_quad(plot['ptsarr'], plot['size'][1], plot['size'][0]))
+                cp_z = pts[:, 2] + self._ctrlpts_offset
+                figure = graph_objs.Scatter3d(
+                    x=pts[:, 0],
+                    y=pts[:, 1],
+                    z=cp_z,
+                    name=plot['name'],
+                    mode='lines+markers',
+                    line=dict(
+                        color=plot['color'],
+                        width=self._config.line_width,
+                        dash='solid'
+                    ),
+                    marker=dict(
+                        color=plot['color'],
+                        size=self._config.line_width
+                    )
+                )
+                plot_data.append(figure)
+
+            # Plot evaluated points
+            if plot['type'] == 'evalpts':
+                pts = np.array(utilities.make_triangle(plot['ptsarr'], plot['size'][1], plot['size'][0]))
                 figure = graph_objs.Scatter3d(
                     x=pts[:, 0],
                     y=pts[:, 1],
