b093e6ca05b59a39aa93b4dcf72544d8d2ea1f0a
Onur R. Bingol <orbingol@gmail.com>
2018-06-25 16:15:50 -0500

    Issue #25 add tests for geometric operations

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -29,3 +29,79 @@ def test_normalize_knot_vector():
     output_kv = [0.0, 0.0, 0.2, 0.3, 0.7, 0.8, 1.0, 1.0]
     to_check = utilities.normalize_knot_vector(input_kv)
     assert to_check == output_kv
+
+
+def test_linspace():
+    start = 5
+    stop = 11
+    num = 4
+    result = [5.0, 7.0, 9.0, 11.0]
+    to_check = utilities.linspace(start, stop, num)
+    assert to_check == result
+
+
+def test_vector_dot():
+    result = 32
+    vec1 = (1, 2, 3)
+    vec2 = (1, 5, 7)
+    to_check = utilities.vector_dot(vec1, vec2)
+    assert to_check == result
+
+
+def test_vector_cross():
+    result = [-1.0, -4.0, 3.0]
+    vec1 = (1, 2, 3)
+    vec2 = (1, 5, 7)
+    to_check = utilities.vector_cross(vec1, vec2)
+    assert to_check == result
+
+
+def test_vector3_normalize():
+    vec = (5, 2.5, 5)
+    result = [0.667, 0.333, 0.667]
+    to_check = utilities.vector_normalize(vec, decimals=3)
+    assert to_check == result
+
+
+def test_vector4_normalize():
+    vec = (5, 2.5, 5, 10)
+    result = [0.4, 0.2, 0.4, 0.8]
+    to_check = utilities.vector_normalize(vec)
+    assert to_check == result
+
+
+def test_vector_generate():
+    pt1 = (0, 0, 0)
+    pt2 = (5, 3, 4)
+    result = [5, 3, 4]
+    result_normalized = [0.707107, 0.424264, 0.565685]
+    to_check = utilities.vector_generate(pt1, pt2)
+    to_check_normalized = utilities.vector_generate(pt1, pt2, normalize=True)
+    assert to_check == result
+    assert to_check_normalized == result_normalized
+
+
+def test_point_translate():
+    pt = (1, 0, 0)
+    vec = (5, 5, 5)
+    result = [6, 5, 5]
+    to_check = utilities.point_translate(pt, vec)
+    assert to_check == result
+
+
+def test_binomial_coefficient1():
+    result = 0.0
+    to_check = utilities.binomial_coefficient(13, 14)
+    assert to_check == result
+
+
+def test_binomial_coefficient2():
+    result = 1.0
+    to_check = utilities.binomial_coefficient(13, 13)
+    assert to_check == result
+
+
+def test_binomial_coefficient3():
+    result = 680.0
+    to_check = utilities.binomial_coefficient(17, 3)
+    assert to_check == result
