c3b619228c37db48011c49f14deea85ff107aafa
Onur R. Bingol <orbingol@gmail.com>
2018-06-26 02:49:50 -0500

    Issue #25 update pickled load-save tests

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -3,14 +3,14 @@
     Released under The MIT License. See LICENSE file for details.
     Copyright (c) 2018 Onur Rauf Bingol
 
-    Tests pickled load and save operations. Requires "pytest" to run.
+    Tests file I/O operations. Requires "pytest" to run.
 """
 
 import os
 from geomdl import BSpline
 
-FILE_NAME = 'testing.pickle'
-SAMPLE_SIZE = 5
+FILE_NAME = 'testing'
+SAMPLE_SIZE = 25
 C_DEGREE = 2
 C_CTRLPTS3D = [[1, 1, 0], [2, 1, -1], [2, 2, 0]]
 C_KV = [0, 0, 0, 1, 1, 1]
@@ -24,18 +24,21 @@ S_KV_U = [0, 0, 0, 1, 1, 1]
 S_KV_V = [0, 0, 0, 1, 1, 1]
 
 
+# Tests pickled load-save operations on curves
 def test_bspline_curve_loadsave():
+    fname = FILE_NAME + ".pickle"
+
     curve_save = BSpline.Curve()
     curve_save.degree = C_DEGREE
     curve_save.ctrlpts = C_CTRLPTS3D
     curve_save.knotvector = C_KV
-    curve_save.save(FILE_NAME)
+    curve_save.save(fname)
 
     curve_load = BSpline.Curve()
-    curve_load.load(FILE_NAME)
+    curve_load.load(fname)
 
     # Remove save file
-    os.remove(FILE_NAME)
+    os.remove(fname)
 
     assert curve_save.degree == curve_load.degree
     assert curve_save.knotvector == curve_load.knotvector
@@ -43,7 +46,10 @@ def test_bspline_curve_loadsave():
     assert curve_save.dimension == curve_load.dimension
 
 
+# Tests pickled load-save operations on surfaces
 def test_bspline_surface_loadsave():
+    fname = FILE_NAME + ".pickle"
+
     surf_save = BSpline.Surface()
     surf_save.degree_u = S_DEGREE_U
     surf_save.degree_v = S_DEGREE_V
@@ -52,13 +58,13 @@ def test_bspline_surface_loadsave():
     surf_save.ctrlpts = S_CTRLPTS
     surf_save.knotvector_u = S_KV_U
     surf_save.knotvector_v = S_KV_V
-    surf_save.save(FILE_NAME)
+    surf_save.save(fname)
 
     surf_load = BSpline.Surface()
-    surf_load.load(FILE_NAME)
+    surf_load.load(fname)
 
     # Remove save file
-    os.remove(FILE_NAME)
+    os.remove(fname)
 
     assert surf_save.degree_u == surf_load.degree_u
     assert surf_save.degree_v == surf_load.degree_v
