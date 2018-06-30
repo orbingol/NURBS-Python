bb79b0841efc3e447a4dcfa45d4ab656fdc48a1f
Onur R. Bingol <orbingol@gmail.com>
2018-06-29 21:46:00 -0500

    Start using pytest fixtures in test_read_write.py

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -7,33 +7,62 @@
 """
 
 import os
+import pytest
 from geomdl import BSpline, NURBS
 from geomdl import exchange
 
 FILE_NAME = 'testing'
 SAMPLE_SIZE = 25
-C_DEGREE = 2
-C_CTRLPTS3D = [[1, 1, 0], [2, 1, -1], [2, 2, 0]]
-C_KV = [0, 0, 0, 1, 1, 1]
 
-S_DEGREE_U = 2
-S_DEGREE_V = 2
-S_CTRLPTS = [[0, 0, 0], [0, 1, 0], [0, 2, -3],
-             [1, 0, 6], [1, 1, 0], [1, 2, 0],
-             [2, 0, 0], [2, 1, 0], [2, 2, 3]]
-S_KV_U = [0, 0, 0, 1, 1, 1]
-S_KV_V = [0, 0, 0, 1, 1, 1]
+
+@pytest.fixture
+def bspline_curve3d():
+    """ Creates a B-Spline 3-dimensional curve instance """
+    curve = BSpline.Curve()
+    curve.degree = 2
+    curve.ctrlpts = [[1, 1, 0], [2, 1, -1], [2, 2, 0]]
+    curve.knotvector = [0, 0, 0, 1, 1, 1]
+    return curve
+
+
+@pytest.fixture
+def bspline_surface():
+    """ Creates a B-Spline surface instance """
+    surf = BSpline.Surface()
+    surf.degree_u = 2
+    surf.degree_v = 2
+    surf.ctrlpts_size_u = 3
+    surf.ctrlpts_size_v = 3
+    surf.ctrlpts = [[0, 0, 0], [0, 1, 0], [0, 2, -3],
+                    [1, 0, 6], [1, 1, 0], [1, 2, 0],
+                    [2, 0, 0], [2, 1, 0], [2, 2, 3]]
+    surf.knotvector_u = [0, 0, 0, 1, 1, 1]
+    surf.knotvector_v = [0, 0, 0, 1, 1, 1]
+    return surf
+
+
+@pytest.fixture
+def nurbs_surface():
+    """ Creates a NURBS surface instance """
+    surf = NURBS.Surface()
+    surf.degree_u = 2
+    surf.degree_v = 2
+    surf.ctrlpts_size_u = 3
+    surf.ctrlpts_size_v = 3
+    surf.ctrlpts = [[0, 0, 0], [0, 1, 0], [0, 2, -3],
+                    [1, 0, 6], [1, 1, 0], [1, 2, 0],
+                    [2, 0, 0], [2, 1, 0], [2, 2, 3]]
+    # use the auto-generated weights vector
+    surf.knotvector_u = [0, 0, 0, 1, 1, 1]
+    surf.knotvector_v = [0, 0, 0, 1, 1, 1]
+    return surf
 
 
 # Tests pickled load-save operations on curves
-def test_bspline_curve_loadsave():
+def test_bspline_curve_loadsave(bspline_curve3d):
     fname = FILE_NAME + ".pickle"
 
-    curve_save = BSpline.Curve()
-    curve_save.degree = C_DEGREE
-    curve_save.ctrlpts = C_CTRLPTS3D
-    curve_save.knotvector = C_KV
-    curve_save.save(fname)
+    bspline_curve3d.save(fname)
 
     curve_load = BSpline.Curve()
     curve_load.load(fname)
@@ -41,25 +70,17 @@ def test_bspline_curve_loadsave():
     # Remove save file
     os.remove(fname)
 
-    assert curve_save.degree == curve_load.degree
-    assert curve_save.knotvector == curve_load.knotvector
-    assert curve_save.ctrlpts == curve_load.ctrlpts
-    assert curve_save.dimension == curve_load.dimension
+    assert bspline_curve3d.degree == curve_load.degree
+    assert bspline_curve3d.knotvector == curve_load.knotvector
+    assert bspline_curve3d.ctrlpts == curve_load.ctrlpts
+    assert bspline_curve3d.dimension == curve_load.dimension
 
 
 # Tests pickled load-save operations on surfaces
-def test_bspline_surface_loadsave():
+def test_bspline_surface_loadsave(bspline_surface):
     fname = FILE_NAME + ".pickle"
 
-    surf_save = BSpline.Surface()
-    surf_save.degree_u = S_DEGREE_U
-    surf_save.degree_v = S_DEGREE_V
-    surf_save.ctrlpts_size_u = 3
-    surf_save.ctrlpts_size_v = 3
-    surf_save.ctrlpts = S_CTRLPTS
-    surf_save.knotvector_u = S_KV_U
-    surf_save.knotvector_v = S_KV_V
-    surf_save.save(fname)
+    bspline_surface.save(fname)
 
     surf_load = BSpline.Surface()
     surf_load.load(fname)
@@ -67,31 +88,22 @@ def test_bspline_surface_loadsave():
     # Remove save file
     os.remove(fname)
 
-    assert surf_save.degree_u == surf_load.degree_u
-    assert surf_save.degree_v == surf_load.degree_v
-    assert surf_save.knotvector_u == surf_load.knotvector_u
-    assert surf_save.knotvector_v == surf_load.knotvector_v
-    assert surf_save.ctrlpts == surf_load.ctrlpts
-    assert surf_save.ctrlpts_size_u == surf_load.ctrlpts_size_u
-    assert surf_save.ctrlpts_size_v == surf_load.ctrlpts_size_v
-    assert surf_save.dimension == surf_load.dimension
+    assert bspline_surface.degree_u == surf_load.degree_u
+    assert bspline_surface.degree_v == surf_load.degree_v
+    assert bspline_surface.knotvector_u == surf_load.knotvector_u
+    assert bspline_surface.knotvector_v == surf_load.knotvector_v
+    assert bspline_surface.ctrlpts == surf_load.ctrlpts
+    assert bspline_surface.ctrlpts_size_u == surf_load.ctrlpts_size_u
+    assert bspline_surface.ctrlpts_size_v == surf_load.ctrlpts_size_v
+    assert bspline_surface.dimension == surf_load.dimension
 
 
 # Tests if the .obj file exists
-def test_export_obj_single():
+def test_export_obj_single(nurbs_surface):
     fname = FILE_NAME + ".obj"
 
-    surf_save = NURBS.Surface()
-    surf_save.degree_u = S_DEGREE_U
-    surf_save.degree_v = S_DEGREE_V
-    surf_save.ctrlpts_size_u = 3
-    surf_save.ctrlpts_size_v = 3
-    surf_save.ctrlpts = S_CTRLPTS
-    surf_save.knotvector_u = S_KV_U
-    surf_save.knotvector_v = S_KV_V
-
-    surf_save.sample_size = SAMPLE_SIZE
-    exchange.export_obj(surf_save, fname)
+    nurbs_surface.sample_size = SAMPLE_SIZE
+    exchange.export_obj(nurbs_surface, fname)
 
     assert os.path.isfile(fname)
     assert os.path.getsize(fname) > 0
@@ -102,20 +114,11 @@ def test_export_obj_single():
 
 
 # Tests if the .off file exists
-def test_export_off_single():
+def test_export_off_single(nurbs_surface):
     fname = FILE_NAME + ".off"
 
-    surf_save = NURBS.Surface()
-    surf_save.degree_u = S_DEGREE_U
-    surf_save.degree_v = S_DEGREE_V
-    surf_save.ctrlpts_size_u = 3
-    surf_save.ctrlpts_size_v = 3
-    surf_save.ctrlpts = S_CTRLPTS
-    surf_save.knotvector_u = S_KV_U
-    surf_save.knotvector_v = S_KV_V
-
-    surf_save.sample_size = SAMPLE_SIZE
-    exchange.export_off(surf_save, fname)
+    nurbs_surface.sample_size = SAMPLE_SIZE
+    exchange.export_off(nurbs_surface, fname)
 
     assert os.path.isfile(fname)
     assert os.path.getsize(fname) > 0
@@ -126,20 +129,11 @@ def test_export_off_single():
 
 
 # Tests if the .stl file exists
-def test_export_stl_single():
+def test_export_stl_single(nurbs_surface):
     fname = FILE_NAME + ".stl"
 
-    surf_save = NURBS.Surface()
-    surf_save.degree_u = S_DEGREE_U
-    surf_save.degree_v = S_DEGREE_V
-    surf_save.ctrlpts_size_u = 3
-    surf_save.ctrlpts_size_v = 3
-    surf_save.ctrlpts = S_CTRLPTS
-    surf_save.knotvector_u = S_KV_U
-    surf_save.knotvector_v = S_KV_V
-
-    surf_save.sample_size = SAMPLE_SIZE
-    exchange.export_stl(surf_save, fname)
+    nurbs_surface.sample_size = SAMPLE_SIZE
+    exchange.export_stl(nurbs_surface, fname)
 
     assert os.path.isfile(fname)
     assert os.path.getsize(fname) > 0
@@ -150,20 +144,11 @@ def test_export_stl_single():
 
 
 # Tests if the .stl file exists (ascii)
-def test_export_stl_ascii_single():
+def test_export_stl_ascii_single(nurbs_surface):
     fname = FILE_NAME + ".stl"
 
-    surf_save = NURBS.Surface()
-    surf_save.degree_u = S_DEGREE_U
-    surf_save.degree_v = S_DEGREE_V
-    surf_save.ctrlpts_size_u = 3
-    surf_save.ctrlpts_size_v = 3
-    surf_save.ctrlpts = S_CTRLPTS
-    surf_save.knotvector_u = S_KV_U
-    surf_save.knotvector_v = S_KV_V
-
-    surf_save.sample_size = SAMPLE_SIZE
-    exchange.export_stl(surf_save, fname, binary=False)
+    nurbs_surface.sample_size = SAMPLE_SIZE
+    exchange.export_stl(nurbs_surface, fname, binary=False)
 
     assert os.path.isfile(fname)
     assert os.path.getsize(fname) > 0
