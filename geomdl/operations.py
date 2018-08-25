0ae696994be02f396d9350eedacf12555d492e6b
Onur Rauf Bingol <orbingol@gmail.com>
2018-08-25 00:59:38 -0500

    Update operations module
    Added the parameter list option back to the module

--- geomdl/operations.py
+++ geomdl/operations.py
@@ -373,178 +373,297 @@ def translate(obj, vec, **kwargs):
 
 
 def tangent(obj, params, **kwargs):
+    """ Evaluates the tangent vector of the curves or surfaces at the input parameter values.
+
+    This function is designed to evaluate tangent vectors of the B-Spline and NURBS shapes at single or
+    multiple parameter values.
+
+    :param obj: input shape
+    :type obj: Abstract.Curve or Abstract.Surface
+    :param params: parameters
+    :type: float, list or tuple
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
     normalize = kwargs.get('normalize', True)
     if isinstance(obj, Abstract.Curve):
-        return _tangent_curve(obj, params, normalize)
+        if isinstance(params, (list, tuple)):
+            return _tangent_curve_single_list(obj, params, normalize)
+        else:
+            return _tangent_curve_single(obj, params, normalize)
     if isinstance(obj, Abstract.Surface):
-        return _tangent_surface(obj, params[0], params[1], normalize)
+        if isinstance(params, (list, tuple)):
+            return _tangent_surface_single_list(obj, params, normalize)
+        else:
+            return _tangent_surface_single(obj, params, normalize)
 
 
 def normal(obj, params, **kwargs):
+    """ Evaluates the normal vector of the curves or surfaces at the input parameter values.
+
+    This function is designed to evaluate normal vectors of the B-Spline and NURBS shapes at single or
+    multiple parameter values.
+
+    :param obj: input shape
+    :type obj: Abstract.Curve or Abstract.Surface
+    :param params: parameters
+    :type: float, list or tuple
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
     normalize = kwargs.get('normalize', True)
     if isinstance(obj, Abstract.Curve):
-        return _normal_curve(obj, params, normalize)
+        if isinstance(params, (list, tuple)):
+            return _normal_curve_single_list(obj, params, normalize)
+        else:
+            return _normal_curve_single(obj, params, normalize)
     if isinstance(obj, Abstract.Surface):
-        return _normal_surface(obj, params[0], params[1], normalize)
+        if isinstance(params, (list, tuple)):
+            return _normal_surface_single_list(obj, params, normalize)
+        else:
+            return _normal_surface_single(obj, params, normalize)
 
 
 def binormal(obj, params, **kwargs):
+    """ Evaluates the binormal vector of the curves or surfaces at the input parameter values.
+
+    This function is designed to evaluate binormal vectors of the B-Spline and NURBS shapes at single or
+    multiple parameter values.
+
+    :param obj: input shape
+    :type obj: Abstract.Curve or Abstract.Surface
+    :param params: parameters
+    :type: float, list or tuple
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
     normalize = kwargs.get('normalize', True)
     if isinstance(obj, Abstract.Curve):
-        return _binormal_curve(obj, params, normalize)
+        if isinstance(obj, Abstract.Curve):
+            if isinstance(params, (list, tuple)):
+                return _binormal_curve_single_list(obj, params, normalize)
+            else:
+                return _binormal_curve_single(obj, params, normalize)
     if isinstance(obj, Abstract.Surface):
         raise NotImplementedError("Binormal vector evaluation for the surfaces is not implemented!")
 
 
 # Evaluates the curve tangent at the given u parameter
-def _tangent_curve(obj, u, normalize):
+def _tangent_curve_single(obj, u, normalize):
     """ Evaluates the curve tangent vector at the given parameter value.
 
     The output returns a list containing the starting point (i.e. origin) of the vector and the vector itself.
 
-    :param u: knot value
+    :param obj: input curve
+    :type obj: Abstract.Curve
+    :param u: parameter
     :type u: float
     :param normalize: if True, the returned vector is converted to a unit vector
     :type normalize: bool
-    :return: a list in the order of "curve point" and "tangent"
-    :rtype: list
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
     """
     # 1st derivative of the curve gives the tangent
     ders = obj.derivatives(u, 1)
 
-    # For readability
     point = ders[0]
-    der_u = ders[1]
+    vector = utilities.vector_normalize(ders[1]) if normalize else ders[1]
 
-    # Normalize the tangent vector
-    if normalize:
-        der_u = utilities.vector_normalize(der_u)
+    return tuple(point), tuple(vector)
 
-    # Return the values
-    return tuple(point), tuple(der_u)
+
+def _tangent_curve_single_list(obj, param_list, normalize):
+    """ Evaluates the curve tangent vectors at the given list of parameter values.
+
+    :param obj: input curve
+    :type obj: Abstract.Curve
+    :param param_list: parameter list
+    :type param_list: list or tuple
+    :param normalize: if True, the returned vector is converted to a unit vector
+    :type normalize: bool
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
+    ret_vector = []
+    for param in param_list:
+        temp = _tangent_curve_single(obj, param, normalize)
+        ret_vector.append(temp)
+    return tuple(ret_vector)
 
 
 # Evaluates the curve normal at the given u parameter
-def _normal_curve(obj, u, normalize):
+def _normal_curve_single(obj, u, normalize):
     """ Evaluates the curve normal vector at the given parameter value.
 
     Curve normal is basically the second derivative of the curve.
     The output returns a list containing the starting point (i.e. origin) of the vector and the vector itself.
 
-    :param u: knot value
+    :param obj: input curve
+    :type obj: Abstract.Curve
+    :param u: parameter
     :type u: float
     :param normalize: if True, the returned vector is converted to a unit vector
     :type normalize: bool
-    :return: a list in the order of "curve point" and "normal"
-    :rtype: list
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
     """
     # 2nd derivative of the curve gives the normal
     ders = obj.derivatives(u, 2)
 
-    # For readability
     point = ders[0]
-    der_u = ders[2]
+    vector = utilities.vector_normalize(ders[2]) if normalize else ders[2]
 
-    # Normalize the normal vector
-    if normalize:
-        der_u = utilities.vector_normalize(der_u)
+    return tuple(point), tuple(vector)
 
-    # Return the values
-    return tuple(point), tuple(der_u)
+
+def _normal_curve_single_list(obj, param_list, normalize):
+    """ Evaluates the curve normal vectors at the given list of parameter values.
+
+    :param obj: input curve
+    :type obj: Abstract.Curve
+    :param param_list: parameter list
+    :type param_list: list or tuple
+    :param normalize: if True, the returned vector is converted to a unit vector
+    :type normalize: bool
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
+    ret_vector = []
+    for param in param_list:
+        temp = _normal_curve_single(obj, param, normalize)
+        ret_vector.append(temp)
+    return tuple(ret_vector)
 
 
 # Evaluates the curve binormal at the given u parameter
-def _binormal_curve(obj, u, normalize):
+def _binormal_curve_single(obj, u, normalize):
     """ Evaluates the curve binormal vector at the given u parameter.
 
     Curve binormal is the cross product of the normal and the tangent vectors.
     The output returns a list containing the starting point (i.e. origin) of the vector and the vector itself.
 
-    :param u: knot value
+    :param obj: input curve
+    :type obj: Abstract.Curve
+    :param u: parameter
     :type u: float
     :param normalize: if True, the returned vector is converted to a unit vector
     :type normalize: bool
-    :return: a list in the order of "curve point" and "binormal"
-    :rtype: list
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
     """
-    tan_vector = _tangent_curve(obj, u, normalize)
-    norm_vector = _normal_curve(obj, u, normalize)
+    # Cross product of tangent and normal vectors gives binormal vector
+    tan_vector = _tangent_curve_single(obj, u, normalize)
+    norm_vector = _normal_curve_single(obj, u, normalize)
 
     point = tan_vector[0]
-    binorm_vector = utilities.vector_cross(tan_vector[1], norm_vector[1])
+    vector = utilities.vector_cross(tan_vector[1], norm_vector[1])
+    vector = utilities.vector_normalize(vector) if normalize else vector
 
-    # Normalize the binormal vector
-    if normalize:
-        binorm_vector = utilities.vector_normalize(binorm_vector)
+    return tuple(point), tuple(vector)
 
-    # Return the values
-    return tuple(point), tuple(binorm_vector)
+
+def _binormal_curve_single_list(obj, param_list, normalize):
+    """ Evaluates the curve binormal vectors at the given list of parameter values.
+
+    :param obj: input curve
+    :type obj: Abstract.Curve
+    :param param_list: parameter list
+    :type param_list: list or tuple
+    :param normalize: if True, the returned vector is converted to a unit vector
+    :type normalize: bool
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
+    ret_vector = []
+    for param in param_list:
+        temp = _binormal_curve_single(obj, param, normalize)
+        ret_vector.append(temp)
+    return tuple(ret_vector)
 
 
 # Evaluates the surface tangent vectors at the given (u,v) parameter
-def _tangent_surface(obj, u, v, normalize):
+def _tangent_surface_single(obj, uv, normalize):
     """ Evaluates the surface tangent vector at the given (u,v) parameter pair.
 
     The output returns a list containing the starting point (i.e., origin) of the vector and the vectors themselves.
 
-    :param u: parameter on the u-direction
-    :type u: float
-    :param v: parameter on the v-direction
-    :type v: float
+    :param obj: input surface
+    :type obj: Abstract.Surface
+    :param uv: (u,v) parameter pair
+    :type uv: list or tuple
     :param normalize: if True, the returned tangent vector is converted to a unit vector
     :type normalize: bool
     :return: A list in the order of "surface point", "derivative w.r.t. u" and "derivative w.r.t. v"
     :rtype: list
     """
     # Tangent is the 1st derivative of the surface
-    skl = obj.derivatives(u, v, 1)
+    skl = obj.derivatives(uv[0], uv[1], 1)
 
-    # Doing this just for readability
     point = skl[0][0]
-    der_u = skl[1][0]
-    der_v = skl[0][1]
+    vector_u = utilities.vector_normalize(skl[1][0]) if normalize else skl[1][0]
+    vector_v = utilities.vector_normalize(skl[0][1]) if normalize else skl[0][1]
 
-    # Normalize the tangent vectors
-    if normalize:
-        der_u = utilities.vector_normalize(der_u)
-        der_v = utilities.vector_normalize(der_v)
+    return tuple(point), tuple(vector_u), tuple(vector_v)
 
-    # Return the list of tangents w.r.t. u and v
-    return tuple(point), tuple(der_u), tuple(der_v)
+
+def _tangent_surface_single_list(obj, param_list, normalize):
+    """ Evaluates the surface tangent vectors at the given list of parameter values.
+
+    :param obj: input surface
+    :type obj: Abstract.Surface
+    :param param_list: parameter list
+    :type param_list: list or tuple
+    :param normalize: if True, the returned vector is converted to a unit vector
+    :type normalize: bool
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
+    ret_vector = []
+    for param in param_list:
+        temp = _tangent_surface_single(obj, param, normalize)
+        ret_vector.append(temp)
+    return tuple(ret_vector)
 
 
 # Evaluates the surface normal vector at the given (u, v) parameter
-def _normal_surface(obj, u, v, normalize):
+def _normal_surface_single(obj, uv, normalize):
     """ Evaluates the surface normal vector at the given (u, v) parameter pair.
 
     The output returns a list containing the starting point (i.e. origin) of the vector and the vector itself.
 
-    :param u: parameter on the u-direction
-    :type u: float
-    :param v: parameter on the v-direction
-    :type v: float
+    :param obj: input surface
+    :type obj: Abstract.Surface
+    :param uv: (u,v) parameter pair
+    :type uv: list or tuple
     :param normalize: if True, the returned normal vector is converted to a unit vector
     :type normalize: bool
     :return: a list in the order of "surface point" and "normal vector"
     :rtype: list
     """
-    # Check u and v parameters are correct for the normal evaluation
-    utilities.check_uv(u, v)
-
     # Take the 1st derivative of the surface
-    skl = obj.derivatives(u, v, 1)
+    skl = obj.derivatives(uv[0], uv[1], 1)
 
-    # For readability
     point = skl[0][0]
-    der_u = skl[1][0]
-    der_v = skl[0][1]
+    vector = utilities.vector_cross(skl[1][0], skl[0][1])
+    vector = utilities.vector_normalize(vector) if normalize else vector
 
-    # Compute normal
-    normal = utilities.vector_cross(der_u, der_v)
+    return tuple(point), tuple(vector)
 
-    if normalize:
-        # Convert normal vector to a unit vector
-        normal = utilities.vector_normalize(tuple(normal))
 
-    # Return the surface normal at the input (u,v) parametric location
-    return tuple(point), tuple(normal)
+def _normal_surface_single_list(obj, param_list, normalize):
+    """ Evaluates the surface normal vectors at the given list of parameter values.
+
+    :param obj: input surface
+    :type obj: Abstract.Surface
+    :param param_list: parameter list
+    :type param_list: list or tuple
+    :param normalize: if True, the returned vector is converted to a unit vector
+    :type normalize: bool
+    :return: a list containing "point" and "vector" pairs
+    :rtype: tuple
+    """
+    ret_vector = []
+    for param in param_list:
+        temp = _normal_surface_single(obj, param, normalize)
+        ret_vector.append(temp)
+    return tuple(ret_vector)
