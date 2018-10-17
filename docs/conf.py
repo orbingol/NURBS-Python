e7664798775dbdfb6fb4bc45db68c6e03a5ef764
Onur Rauf Bingol <orbingol@gmail.com>
2018-10-17 17:45:59 -0500

    Add enum module to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosummary']
 
 # List of modules to be mocked up
-autodoc_mock_imports = ['numpy', 'matplotlib', 'mpl_toolkits', 'plotly']
+autodoc_mock_imports = ['numpy', 'matplotlib', 'mpl_toolkits', 'plotly', 'enum']
 
 # # Order of functions in the documentation; default is 'alphabetical'
 # autodoc_member_order = 'bysource'
