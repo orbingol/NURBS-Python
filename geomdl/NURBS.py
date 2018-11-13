7202f96e6fa46a74e13ed48837eaf9ca2b6358f9
Onur Rauf Bingol <orbingol@gmail.com>
2018-11-12 20:59:33 -0600

    Add example links to NURBS curve and surface docstrings

--- geomdl/NURBS.py
+++ geomdl/NURBS.py
@@ -28,6 +28,8 @@ class Curve(BSpline.Curve):
     Notes:
         * Please see the :py:class:`.Abstract.Surface()` documentation for details.
         * This class sets the *FindSpan* implementation to Linear Search by default.
+
+    Please see ``curve2d\ex_curve04.py`` in the :doc:`Examples repository <examples_repo>` for a NURBS curve example.
     """
 
     def __init__(self, **kwargs):
@@ -156,6 +158,8 @@ class Surface(BSpline.Surface):
     Notes:
         * Please see the :py:class:`.Abstract.Surface()` documentation for details.
         * This class sets the *FindSpan* implementation to Linear Search by default.
+
+    Please see ``surface\ex_surface03.py`` in the :doc:`Examples repository <examples_repo>` for a NURBS surface example.
     """
 
     def __init__(self, **kwargs):
