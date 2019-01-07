a067b3fa0fdb0a1d6e25d3cd7553904795efcd70
Onur Rauf Bingol <orbingol@gmail.com>
2019-01-06 18:44:51 -0600

    Add performance testing to TravisCI

---
+++ .travisci/curve_sequential_pure.py
@@ -0,0 +1,33 @@
+# Performance testing on the TravisCI
+import os
+import sys
+import platform
+import timeit
+from geomdl import *
+
+
+# Setup test
+def setup_test():
+    ctrlpts = [[5.0, 15.0, 0.0], [10.0, 25.0, 5.0], [20.0, 20.0, 10.0], [15.0, -5.0, 15.0], [7.5, 10.0, 20.0],
+               [12.5, 15.0, 25.0], [15.0, 0.0, 30.0], [5.0, -10.0, 35.0], [10.0, 15.0, 40.0], [5.0, 15.0, 30.0]]
+
+    ns = NURBS.Curve()
+    ns.degree = 3
+    ns.ctrlpts = ctrlpts
+    ns.knotvector = utilities.generate_knot_vector(ns.degree, ns.ctrlpts_size)
+    ns.sample_size = 16384
+
+    return ns
+
+
+# Setup number of executions
+number = os.environ['GEOMDL_PERF_NUMBER'] if 'GEOMDL_PERF_NUMBER' in os.environ else 50
+repeat = os.environ['GEOMDL_PERF_REPEAT'] if 'GEOMDL_PERF_REPEAT' in os.environ else 3
+version = os.environ['TRAVIS_PYTHON_VERSION'] if 'TRAVIS_PYTHON_VERSION' in os.environ else ".".join(str(v) for v in sys.version_info[0:3])
+
+# Run timeit
+res = timeit.repeat(setup="from __main__ import setup_test; ns=setup_test()", stmt="ns.evaluate()",
+                    repeat=repeat, number=number)
+
+# Print results
+print(__file__, "on", platform.python_implementation(), str(version), ">>", str(number), "loops, best of", str(repeat), "is", str(min(res)), "seconds per loop")
