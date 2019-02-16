c9339ed5ace09936e165ae6a048a41e482c917b0
Onur Rauf Bingol <orbingol@gmail.com>
2019-02-15 21:48:29 -0600

    Update knot insertion and add support for volumes

--- geomdl/operations.py
+++ geomdl/operations.py
@@ -66,11 +66,11 @@ def insert_knot(obj, param, num, **kwargs):
 
             # Compute new control points
             cpts = obj.ctrlptsw if obj.rational else obj.ctrlpts
-            ctrlpts_new = helpers.knot_insertion(obj.degree, obj.knotvector, cpts, param[0],
-                                                 num=num[0], s=s, span=span)
+            cpts_tmp = helpers.knot_insertion(obj.degree, obj.knotvector, cpts, param[0],
+                                              num=num[0], s=s, span=span)
 
             # Update curve
-            obj.set_ctrlpts(ctrlpts_new)
+            obj.set_ctrlpts(cpts_tmp)
             obj.knotvector = kv_new
 
     # Start surface knot insertion
@@ -92,16 +92,16 @@ def insert_knot(obj, param, num, **kwargs):
             kv_u = helpers.knot_insertion_kv(obj.knotvector_u, param[0], span_u, num[0])
 
             # Get curves
-            ctrlpts_new = []
+            cpts_tmp = []
             cpts = obj.ctrlptsw if obj.rational else obj.ctrlpts
             for v in range(obj.ctrlpts_size_v):
                 ccu = [cpts[v + (obj.ctrlpts_size_v * u)] for u in range(obj.ctrlpts_size_u)]
                 ctrlpts_tmp = helpers.knot_insertion(obj.degree_u, obj.knotvector_u, ccu, param[0],
                                                      num=num[0], s=s_u, span=span_u)
-                ctrlpts_new += ctrlpts_tmp
+                cpts_tmp += ctrlpts_tmp
 
             # Update the surface after knot insertion
-            obj.set_ctrlpts(compatibility.flip_ctrlpts_u(ctrlpts_new, obj.ctrlpts_size_u + num[0], obj.ctrlpts_size_v),
+            obj.set_ctrlpts(compatibility.flip_ctrlpts_u(cpts_tmp, obj.ctrlpts_size_u + num[0], obj.ctrlpts_size_v),
                             obj.ctrlpts_size_u + num[0], obj.ctrlpts_size_v)
             obj.knotvector_u = kv_u
 
@@ -122,22 +122,148 @@ def insert_knot(obj, param, num, **kwargs):
             kv_v = helpers.knot_insertion_kv(obj.knotvector_v, param[1], span_v, num[1])
 
             # Get curves
-            ctrlpts_new = []
+            cpts_tmp = []
             cpts = obj.ctrlptsw if obj.rational else obj.ctrlpts
             for u in range(obj.ctrlpts_size_u):
                 ccv = [cpts[v + (obj.ctrlpts_size_v * u)] for v in range(obj.ctrlpts_size_v)]
                 ctrlpts_tmp = helpers.knot_insertion(obj.degree_v, obj.knotvector_v, ccv, param[1],
                                                      num=num[1], s=s_v, span=span_v)
-                ctrlpts_new += ctrlpts_tmp
+                cpts_tmp += ctrlpts_tmp
 
             # Update the surface after knot insertion
-            obj.set_ctrlpts(ctrlpts_new, obj.ctrlpts_size_u, obj.ctrlpts_size_v + num[1])
+            obj.set_ctrlpts(cpts_tmp, obj.ctrlpts_size_u, obj.ctrlpts_size_v + num[1])
             obj.knotvector_v = kv_v
 
     # Start volume knot insertion
     if isinstance(obj, abstract.Volume):
-        # Not implemented yet
-        pass
+        # u-direction
+        if param[0] is not None and num[0] > 0:
+            # Find knot multiplicity
+            s_u = helpers.find_multiplicity(param[0], obj.knotvector_u)
+
+            # Check if it is possible add that many number of knots
+            if check_num and num[0] > obj.degree_u - s_u:
+                raise GeomdlException("Knot " + str(param[0]) + " cannot be inserted " + str(num[0]) + " times (u-dir)",
+                                      data=dict(knot=param[0], num=num[0], multiplicity=s_u))
+
+            # Find knot span
+            span_u = helpers.find_span_linear(obj.degree_u, obj.knotvector_u, obj.ctrlpts_size_u, param[0])
+
+            # Compute new knot vector
+            kv_u = helpers.knot_insertion_kv(obj.knotvector_u, param[0], span_u, num[0])
+
+            # Use Pw if rational
+            cpts = obj.ctrlptsw if obj.rational else obj.ctrlpts
+
+            # Construct 2-dimensional structure
+            cpt2d = []
+            for u in range(obj.ctrlpts_size_u):
+                temp_surf = []
+                for w in range(obj.ctrlpts_size_w):
+                    for v in range(obj.ctrlpts_size_v):
+                        temp_pt = cpts[v + (u * obj.ctrlpts_size_v) + (w * obj.ctrlpts_size_u * obj.ctrlpts_size_v)]
+                        temp_surf.append(temp_pt)
+                cpt2d.append(temp_surf)
+
+            # Compute new control points
+            ctrlpts_tmp = helpers.knot_insertion(obj.degree_u, obj.knotvector_u, cpt2d, param[0],
+                                                 num=num[0], s=s_u, span=span_u)
+
+            # Flatten to 1-dimensional structure
+            ctrlpts_new = []
+            for w in range(obj.ctrlpts_size_w):
+                for u in range(obj.ctrlpts_size_u + num[0]):
+                    for v in range(obj.ctrlpts_size_v):
+                        temp_pt = ctrlpts_tmp[u][v + (w * obj.ctrlpts_size_v)]
+                        ctrlpts_new.append(temp_pt)
+
+            # Update the volume after knot insertion
+            obj.set_ctrlpts(ctrlpts_new, obj.ctrlpts_size_u + num[0], obj.ctrlpts_size_v, obj.ctrlpts_size_w)
+            obj.knotvector_u = kv_u
+
+        # v-direction
+        if param[1] is not None and num[1] > 0:
+            # Find knot multiplicity
+            s_v = helpers.find_multiplicity(param[1], obj.knotvector_v)
+
+            # Check if it is possible add that many number of knots
+            if check_num and num[1] > obj.degree_v - s_v:
+                raise GeomdlException("Knot " + str(param[1]) + " cannot be inserted " + str(num[1]) + " times (v-dir)",
+                                      data=dict(knot=param[1], num=num[1], multiplicity=s_v))
+
+            # Find knot span
+            span_v = helpers.find_span_linear(obj.degree_v, obj.knotvector_v, obj.ctrlpts_size_v, param[1])
+
+            # Compute new knot vector
+            kv_v = helpers.knot_insertion_kv(obj.knotvector_v, param[1], span_v, num[1])
+
+            # Use Pw if rational
+            cpts = obj.ctrlptsw if obj.rational else obj.ctrlpts
+
+            # Construct 2-dimensional structure
+            cpt2d = []
+            for v in range(obj.ctrlpts_size_v):
+                temp_surf = []
+                for w in range(obj.ctrlpts_size_w):
+                    for u in range(obj.ctrlpts_size_u):
+                        temp_pt = cpts[v + (u * obj.ctrlpts_size_v) + (w * obj.ctrlpts_size_u * obj.ctrlpts_size_v)]
+                        temp_surf.append(temp_pt)
+                cpt2d.append(temp_surf)
+
+            # Compute new control points
+            ctrlpts_tmp = helpers.knot_insertion(obj.degree_v, obj.knotvector_v, cpt2d, param[1],
+                                                 num=num[1], s=s_v, span=span_v)
+
+            # Flatten to 1-dimensional structure
+            ctrlpts_new = []
+            for w in range(obj.ctrlpts_size_w):
+                for u in range(obj.ctrlpts_size_u):
+                    for v in range(obj.ctrlpts_size_v + num[1]):
+                        temp_pt = ctrlpts_tmp[v][u + (w * obj.ctrlpts_size_u)]
+                        ctrlpts_new.append(temp_pt)
+
+            # Update the volume after knot insertion
+            obj.set_ctrlpts(ctrlpts_new, obj.ctrlpts_size_u, obj.ctrlpts_size_v + num[1], obj.ctrlpts_size_w)
+            obj.knotvector_v = kv_v
+
+        # w-direction
+        if param[2] is not None and num[2] > 0:
+            # Find knot multiplicity
+            s_w = helpers.find_multiplicity(param[2], obj.knotvector_w)
+
+            # Check if it is possible add that many number of knots
+            if check_num and num[2] > obj.degree_w - s_w:
+                raise GeomdlException("Knot " + str(param[2]) + " cannot be inserted " + str(num[2]) + " times (w-dir)",
+                                      data=dict(knot=param[2], num=num[2], multiplicity=s_w))
+
+            # Find knot span
+            span_w = helpers.find_span_linear(obj.degree_w, obj.knotvector_w, obj.ctrlpts_size_w, param[2])
+
+            # Compute new knot vector
+            kv_w = helpers.knot_insertion_kv(obj.knotvector_w, param[2], span_w, num[2])
+
+            # Use Pw if rational
+            cpts = obj.ctrlptsw if obj.rational else obj.ctrlpts
+
+            # Construct 2-dimensional structure
+            cpt2d = []
+            for w in range(obj.ctrlpts_size_w):
+                temp_surf = [cpts[uv + (w * obj.ctrlpts_size_u * obj.ctrlpts_size_v)] for uv in
+                             range(obj.ctrlpts_size_u * obj.ctrlpts_size_v)]
+                cpt2d.append(temp_surf)
+
+            # Compute new control points
+            ctrlpts_tmp = helpers.knot_insertion(obj.degree_w, obj.knotvector_w, cpt2d, param[2],
+                                                 num=num[2], s=s_w, span=span_w)
+
+            # Flatten to 1-dimensional structure
+            ctrlpts_new = []
+            for w in range(obj.ctrlpts_size_w + num[2]):
+                ctrlpts_new += ctrlpts_tmp[w]
+
+            # Update the volume after knot insertion
+            obj.set_ctrlpts(ctrlpts_new, obj.ctrlpts_size_u, obj.ctrlpts_size_v, obj.ctrlpts_size_w + num[2])
+            obj.knotvector_w = kv_w
 
     # Return updated spline geometry
     return obj
@@ -257,8 +383,53 @@ def remove_knot(obj, param, num, **kwargs):
 
     # Start volume knot removal
     if isinstance(obj, abstract.Volume):
-        # Not implemented yet
-        pass
+        # u-direction
+        if param[0] is not None and num[0] > 0:
+            # Find knot multiplicity
+            s_u = helpers.find_multiplicity(param[0], obj.knotvector_u)
+
+            # Check if it is possible add that many number of knots
+            if check_num and num[0] > s_u:
+                raise GeomdlException("Knot " + str(param[0]) + " cannot be removed " + str(num[0]) + " times (u-dir)",
+                                      data=dict(knot=param[0], num=num[0], multiplicity=s_u))
+
+            # Find knot span
+            span_u = helpers.find_span_linear(obj.degree_u, obj.knotvector_u, obj.ctrlpts_size_u, param[0])
+
+            # Get surfaces
+            pass
+
+        # v-direction
+        if param[1] is not None and num[1] > 0:
+            # Find knot multiplicity
+            s_v = helpers.find_multiplicity(param[1], obj.knotvector_v)
+
+            # Check if it is possible add that many number of knots
+            if check_num and num[1] > s_v:
+                raise GeomdlException("Knot " + str(param[1]) + " cannot be removed " + str(num[1]) + " times (v-dir)",
+                                      data=dict(knot=param[1], num=num[1], multiplicity=s_v))
+
+            # Find knot span
+            span_v = helpers.find_span_linear(obj.degree_v, obj.knotvector_v, obj.ctrlpts_size_v, param[1])
+
+            # Get surfaces
+            pass
+
+        # w-direction
+        if param[2] is not None and num[2] > 0:
+            # Find knot multiplicity
+            s_w = helpers.find_multiplicity(param[2], obj.knotvector_w)
+
+            # Check if it is possible add that many number of knots
+            if check_num and num[2] > s_w:
+                raise GeomdlException("Knot " + str(param[2]) + " cannot be removed " + str(num[2]) + " times (v-dir)",
+                                      data=dict(knot=param[2], num=num[2], multiplicity=s_w))
+
+            # Find knot span
+            span_w = helpers.find_span_linear(obj.degree_w, obj.knotvector_w, obj.ctrlpts_size_w, param[2])
+
+            # Get surfaces
+            pass
 
     # Return updated spline geometry
     return obj
