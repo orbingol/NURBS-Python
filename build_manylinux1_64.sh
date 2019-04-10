21b76752a5e9c33938031db0807a593fb59c056a
Onur Rauf Bingol <orbingol@gmail.com>
2019-04-10 01:10:13 -0500

    Add bash scripts for building manylinux1 packages

---
+++ build_manylinux1_64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Builds "manylinux1" packages for geomdl
+# Setup Docker: http://support.divio.com/local-development/docker/how-to-use-a-directory-outside-cusers-with-docker-toolboxdocker-for-windows
+# Run Docker: docker run -it -v /home/docker/projects:/io quay.io/pypa/manylinux1_x86_64 /bin/bash
+
+MODULE_NAME="geomdl"
+PLATFORM_NAME="linux_x86_64"
+PYTHON_VERSIONS="cp27-cp27m cp35-cp35m cp36-cp36m cp37-cp37m"
+GIT_TAG_FULL=`git describe --tags`
+GIT_TAG="${GIT_TAG_FULL:1}"
+
+for pyver in $PYTHON_VERSIONS
+do
+  /opt/python/$pyver/bin/pip install cython
+  /opt/python/$pyver/bin/python setup.py bdist_wheel --use-cython
+  auditwheel repair dist/$MODULE_NAME-$GIT_TAG-$pyver-$PLATFORM_NAME.whl
+  /opt/python/$pyver/bin/python setup.py clean --all
+done
