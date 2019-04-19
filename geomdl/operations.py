52278360dd3094326e87b3716d745cfdf57f7acf
Onur R. Bingol <orbingol@gmail.com>
2019-04-19 18:04:56 -0500

    Add general support for spatial dim elevation

--- geomdl/operations.py
+++ geomdl/operations.py
@@ -874,6 +874,42 @@ def degree_operations(obj, param, **kwargs):
 
 
 @export
+def add_dimension(obj, **kwargs):
+    """ Elevates the spatial dimension of the spline geometry.
+
+    If you pass ``inplace=True`` keyword argument, the input will be updated. Otherwise, this function does not
+    change the input but returns a new instance with the updated data.
+
+    :param obj: spline geometry
+    :type obj: abstract.SplineGeometry
+    :return: updated spline geometry
+    :rtype: abstract.SplineGeometry
+    """
+    if not isinstance(obj, abstract.SplineGeometry):
+        raise GeomdlException("Can only operate on spline geometry objects")
+
+    # Keyword arguments
+    inplace = kwargs.get('inplace', False)
+    array_init = kwargs.get('array_init', [[] for _ in range(len(obj.ctrlpts))])
+    offset_value = kwargs.get('offset', 0.0)
+
+    # Update control points
+    new_ctrlpts = array_init
+    for idx, point in enumerate(obj.ctrlpts):
+        temp = [float(p) for p in point[0:obj.dimension]]
+        temp.append(offset_value)
+        new_ctrlpts[idx] = temp
+
+    if inplace:
+        obj.ctrlpts = new_ctrlpts
+        return obj
+    else:
+        ret = copy.deepcopy(obj)
+        ret.ctrlpts = new_ctrlpts
+        return ret
+
+
+@export
 def split_curve(obj, param, **kwargs):
     """ Splits the curve at the input parametric coordinate.
 
@@ -1040,45 +1076,6 @@ def length_curve(obj):
     for idx in range(num_evalpts - 1):
         length += linalg.point_distance(evalpts[idx], evalpts[idx + 1])
     return length
-
-
-@export
-def add_dimension(obj, **kwargs):
-    """ Converts x-dimensional curve to a (x+1)-dimensional curve.
-
-    If you pass ``inplace=True`` keyword argument, the input shape will be updated. Otherwise, this function does not
-    change the input shape but returns a new instance of the same shape with the updated data.
-
-    Useful when converting a 2-dimensional curve to a 3-dimensional curve. Please note that this function does not
-    change the parametric dimension but only the spatial dimension.
-
-    :param obj: Curve
-    :type obj: abstract.Curve
-    :return: updated Curve
-    :rtype: BSpline.Curve or NURBS.Curve
-    """
-    if not isinstance(obj, abstract.Curve):
-        raise GeomdlException("Input shape must be an instance of abstract.Curve class")
-
-    # Keyword arguments
-    inplace = kwargs.get('inplace', False)
-    array_init = kwargs.get('array_init', [[] for _ in range(len(obj.ctrlpts))])
-    offset_value = kwargs.get('offset', 0.0)
-
-    # Update control points
-    new_ctrlpts = array_init
-    for idx, point in enumerate(obj.ctrlpts):
-        temp = [float(p) for p in point[0:obj.dimension]]
-        temp.append(offset_value)
-        new_ctrlpts[idx] = temp
-
-    if inplace:
-        obj.ctrlpts = new_ctrlpts
-        return obj
-    else:
-        ret = copy.deepcopy(obj)
-        ret.ctrlpts = new_ctrlpts
-        return ret
 
 
 @export
