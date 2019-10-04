5f9f3e8e4391114d9ea6d6cdd7c64d125653a11a
Onur Rauf Bingol <orbingol@gmail.com>
2019-10-04 13:19:14 -0500

    Refactoring common functions and add six.py

--- geomdl/base.py
+++ geomdl/base.py
@@ -1,9 +1,19 @@
+"""
+.. module:: abstract
+    :platform: Unix, Windows
+    :synopsis: Provides abstract base classes for representing the geometries
+
+.. moduleauthor:: Onur Rauf Bingol <orbingol@gmail.com>
+
+"""
+
 from __future__ import absolute_import
 
-from abc import *
+import abc
 from . import _utilities as utl
 
-@utl.add_metaclass(ABCMeta)
+
+@utl.add_metaclass(abc.ABCMeta)
 class GeomdlSequence(object):
     """ Abstract base for supported input types
 
@@ -13,7 +23,8 @@ class GeomdlSequence(object):
     import numpy as np
     GeomdlSequence.register(np.ndarray)
     """
+    pass
+
 
 GeomdlSequence.register(list)
 GeomdlSequence.register(tuple)
-
