0e92dddf26016cd573f6361533d940a9967c03bd
Onur Rauf Bingol <orbingol@gmail.com>
2019-10-04 21:41:01 -0500

    Update imports and type checks

--- geomdl/ray.py
+++ geomdl/ray.py
@@ -7,9 +7,8 @@
 
 """
 import sys
-
-from . import abc, linalg
-from ._utilities import export
+from . import linalg
+from .base import export, GeomdlTypeSequence
 
 
 @export
@@ -27,9 +26,9 @@ class Ray(object):
     """
     def __init__(self, point1, point2):
         super(Ray, self).__init__()
-        if not isinstance(point1, abc.GeomdlSequence):
+        if not isinstance(point1, GeomdlTypeSequence):
             raise TypeError("Point 1 must be a list or a tuple")
-        if not isinstance(point2, abc.GeomdlSequence):
+        if not isinstance(point2, GeomdlTypeSequence):
             raise TypeError("Point 2 must be a list or a tuple")
         if len(point1) != len(point2):
             raise ValueError("THe dimensions of the input points must be equal")
