79f83e894c98769d54671c88e5e0da03f5196263
Onur Rauf Bingol <orbingol@gmail.com>
2020-01-29 20:00:05 -0600

    Variable name update

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -14,7 +14,7 @@ GEOMDL_DELTA = 0.001
 
 
 @fixture
-def splcurve():
+def bsplcurve():
     """ Creates a spline Curve """
     curve = BSpline.Curve()
     curve.degree = 3
@@ -23,23 +23,23 @@ def splcurve():
     return curve
 
 
-def test_bsplcurve_name(splcurve):
-    splcurve.name = "Curve Testing"
-    assert splcurve.name == "Curve Testing"
+def test_bsplcurve_name(bsplcurve):
+    bsplcurve.name = "Curve Testing"
+    assert bsplcurve.name == "Curve Testing"
 
 
-def test_bsplcurve_degree(splcurve):
-    assert splcurve.degree[0] == 3
+def test_bsplcurve_degree(bsplcurve):
+    assert bsplcurve.degree[0] == 3
 
 
-def test_bsplcurve_ctrlpts(splcurve):
-    assert all([x == y for x, y in zip(splcurve.ctrlpts[4], [45.0, 10.0])])
-    assert all([x == y for x, y in zip(splcurve.ctrlpts[2], [20.0, 15.0])])
-    assert splcurve.dimension == 2
+def test_bsplcurve_ctrlpts(bsplcurve):
+    assert all([x == y for x, y in zip(bsplcurve.ctrlpts[4], [45.0, 10.0])])
+    assert all([x == y for x, y in zip(bsplcurve.ctrlpts[2], [20.0, 15.0])])
+    assert bsplcurve.dimension == 2
 
 
-def test_bsplcurve_knot_vector(splcurve):
-    assert all([x == y for x, y in zip(splcurve.knotvector[0], [0.0, 0.0, 0.0, 0.0, 0.33, 0.66, 1.0, 1.0, 1.0, 1.0])])
+def test_bsplcurve_knot_vector(bsplcurve):
+    assert all([x == y for x, y in zip(bsplcurve.knotvector[0], [0.0, 0.0, 0.0, 0.0, 0.33, 0.66, 1.0, 1.0, 1.0, 1.0])])
 
 
 @mark.parametrize("param, res", [
@@ -49,18 +49,18 @@ def test_bsplcurve_knot_vector(splcurve):
     (0.6, (32.143, 14.328)),
     (1.0, (50.0, 5.0))
 ])
-def test_bsplcurve2d_eval(splcurve, param, res):
-    evalpt = splcurve.evaluate_single(param)
+def test_bsplcurve2d_eval(bsplcurve, param, res):
+    evalpt = bsplcurve.evaluate_single(param)
 
     assert abs(evalpt[0] - res[0]) < GEOMDL_DELTA
     assert abs(evalpt[1] - res[1]) < GEOMDL_DELTA
 
 
 @mark.parametrize("param", [0.11, 0.35, 0.67, 0.99])
-def test_bsplcurve2d_deriv(splcurve, param):
-    der1 = splcurve.derivatives(param=(param,), order=2)
-    splcurve.evaluator = evaluators.CurveEvaluator2()
-    der2 = splcurve.derivatives(param=(param,), order=2)
+def test_bsplcurve2d_deriv(bsplcurve, param):
+    der1 = bsplcurve.derivatives(param=(param,), order=2)
+    bsplcurve.evaluator = evaluators.CurveEvaluator2()
+    der2 = bsplcurve.derivatives(param=(param,), order=2)
 
     assert abs(der1[0][0] - der2[0][0]) < GEOMDL_DELTA
     assert abs(der1[0][1] - der2[0][1]) < GEOMDL_DELTA
@@ -71,11 +71,11 @@ def test_bsplcurve2d_deriv(splcurve, param):
 
 
 @mark.parametrize("param", [0.11, 0.35, 0.67, 0.99])
-def test_bsplcurve2d_deriv_eval(splcurve, param):
-    evalpt = splcurve.evaluate_single(param)
-    der1 = splcurve.derivatives(param=param)
-    splcurve.evaluator = evaluators.CurveEvaluator2()
-    der2 = splcurve.derivatives(param=param)
+def test_bsplcurve2d_deriv_eval(bsplcurve, param):
+    evalpt = bsplcurve.evaluate_single(param)
+    der1 = bsplcurve.derivatives(param=param)
+    bsplcurve.evaluator = evaluators.CurveEvaluator2()
+    der2 = bsplcurve.derivatives(param=param)
 
     assert abs(der1[0][0] - evalpt[0]) < GEOMDL_DELTA
     assert abs(der1[0][1] - evalpt[1]) < GEOMDL_DELTA
