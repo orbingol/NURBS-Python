b686df29117d33f85ba8016c321c79b7692cc395
Onur Rauf Bingol <orbingol@gmail.com>
2020-02-01 13:29:11 -0600

    Make render work with multi geometries (lists, containers, etc.)

--- geomdl/render.py
+++ geomdl/render.py
@@ -11,11 +11,11 @@ from . import tessellate
 from . import voxelize
 
 RENDER_OPTIONS = dict(
-    cpcolor='blue',
-    evalcolor='green',
-    bboxcolor='darkorange',
-    trimcolor='black',
-    colormap=None,
+    cpcolor=['blue'],
+    evalcolor=['green'],
+    bboxcolor=['darkorange'],
+    trimcolor=['black'],
+    colormap=list(),
     filename=None,
     plot=True,
     extras=list(),
@@ -35,105 +35,107 @@ def render(spg, vism, **kwargs):
     # Clear the visualization component
     vism.clear()
 
-    # Add control points as points
-    if vism.mconf['ctrlpts'] == 'points':
-        vism.add(
-            ptsarr=spg.ctrlpts.points,
-            name="control points",
-            color=op['cpcolor'],
-            plot_type='ctrlpts'
-        )
-
-    # Add control points as quads
-    if vism.mconf['ctrlpts'] == 'quads':
-        qtsl = op['config_tsl_quad'] if op['config_tsl_quad'] is not None else tessellate.QuadTessellate()
-        qtsl.tessellate(spg.ctrlpts.points, size_u=spg.ctrlpts_size.u, size_v=spg.ctrlpts_size.v)
-        vism.add(
-            ptsarr=[qtsl.vertices, qtsl.faces],
-            name="control points",
-            color=op['cpcolor'],
-            plot_type='ctrlpts'
-        )
-
-    # Add evaluated points as points
-    if vism.mconf['evalpts'] == 'points':
-        vism.add(
-            ptsarr=spg.evalpts,
-            name=spg.name,
-            color=op['evalcolor'],
-            plot_type='evalpts'
-        )
-
-    # Add evaluated points as quads
-    if vism.mconf['evalpts'] == 'quads':
-        qtsl = op['config_tsl_quad'] if op['config_tsl_quad'] is not None else tessellate.QuadTessellate()
-        qtsl.tessellate(spg.evalpts, size_u=spg.sample_size[0], size_v=spg.sample_size[1])
-        vism.add(
-            ptsarr=[qtsl.vertices, qtsl.faces],
-            name=spg.name,
-            color=op['evalcolor'],
-            plot_type='evalpts'
-        )
-
-    # Add evaluated points as vertices and triangles
-    if vism.mconf['evalpts'] == 'triangles':
-        tsl = op['config_tsl_tri'] if op['config_tsl_tri'] is not None else tessellate.TrimTessellate()
-        tsl.tessellate(spg.evalpts, size_u=spg.sample_size[0], size_v=spg.sample_size[1])
-        vism.add(
-            ptsarr=[tsl.vertices, tsl.faces],
-            name=spg.name,
-            color=op['evalcolor'],
-            plot_type='evalpts'
-        )
-
-    # Add evaluated points as voxels
-    if vism.mconf['evalpts'] == 'voxels':
-        grid, filled = voxelize.voxelize(spg, **kwargs)
-        faces = voxelize.convert_bb_to_faces(grid)
-        vism.add(
-            ptsarr=[grid, faces, filled],
-            name=spg.name,
-            color=op['evalcolor'],
-            plot_type='evalpts'
-        )
-
-    # Add trim curves
-    if spg.trims:
-        for idx, trim in enumerate(spg.trims):
+    ssz = len(spg)
+    for si, s in enumerate(spg):
+        # Add control points as points
+        if vism.mconf['ctrlpts'] == 'points':
             vism.add(
-                ptsarr=spg.evaluate_list(trim.evalpts),
-                name="Trim Curve " + str(idx + 1),
-                color=op['trimcolor'],
-                plot_type='trimcurve'
+                ptsarr=s.ctrlpts.points,
+                name="control points" if ssz == 1 else  "control points {}".format(si + 1),
+                color=op['cpcolor'][si],
+                plot_type='ctrlpts'
             )
 
-    # Add bounding box
-    vism.add(
-        ptsarr=spg.bbox,
-        name="Bounding Box",
-        color=op['bboxcolor'],
-        plot_type='bbox'
-    )
+        # Add control points as quads
+        if vism.mconf['ctrlpts'] == 'quads':
+            qtsl = op['config_tsl_quad'] if op['config_tsl_quad'] is not None else tessellate.QuadTessellate()
+            qtsl.tessellate(s.ctrlpts.points, size_u=s.ctrlpts_size[0], size_v=s.ctrlpts_size[1])
+            vism.add(
+                ptsarr=[qtsl.vertices, qtsl.faces],
+                name="control points" if ssz == 1 else "control points {}".format(si + 1),
+                color=op['cpcolor'][si],
+                plot_type='ctrlpts'
+            )
 
-    # Add user plots
-    for ep in op['extras']:
+        # Add evaluated points as points
+        if vism.mconf['evalpts'] == 'points':
+            vism.add(
+                ptsarr=s.evalpts,
+                name=s.name if ssz == 1 else "geometry {}".format(si + 1),
+                color=op['evalcolor'][si],
+                plot_type='evalpts'
+            )
+
+        # Add evaluated points as quads
+        if vism.mconf['evalpts'] == 'quads':
+            qtsl = op['config_tsl_quad'] if op['config_tsl_quad'] is not None else tessellate.QuadTessellate()
+            qtsl.tessellate(s.evalpts, size_u=s.sample_size[0], size_v=s.sample_size[1])
+            vism.add(
+                ptsarr=[qtsl.vertices, qtsl.faces],
+                name=s.name if ssz == 1 else "geometry {}".format(si + 1),
+                color=op['evalcolor'][si],
+                plot_type='evalpts'
+            )
+
+        # Add evaluated points as vertices and triangles
+        if vism.mconf['evalpts'] == 'triangles':
+            tsl = op['config_tsl_tri'] if op['config_tsl_tri'] is not None else tessellate.TrimTessellate()
+            tsl.tessellate(s.evalpts, size_u=s.sample_size[0], size_v=s.sample_size[1])
+            vism.add(
+                ptsarr=[tsl.vertices, tsl.faces],
+                name=s.name if ssz == 1 else "geometry {}".format(si + 1),
+                color=op['evalcolor'][si],
+                plot_type='evalpts'
+            )
+
+        # Add evaluated points as voxels
+        if vism.mconf['evalpts'] == 'voxels':
+            grid, filled = voxelize.voxelize(s, **kwargs)
+            faces = voxelize.convert_bb_to_faces(grid)
+            vism.add(
+                ptsarr=[grid, faces, filled],
+                name=s.name if ssz == 1 else "geometry {}".format(si + 1),
+                color=op['evalcolor'][si],
+                plot_type='evalpts'
+            )
+
+        # Add trim curves
+        if spg.trims:
+            for idx, trim in enumerate(spg.trims):
+                vism.add(
+                    ptsarr=s.evaluate_list(trim.evalpts),
+                    name="trim {}".format(idx + 1) if ssz == 1 else "trim {} for geometry {}".format(idx + 1, si + 1),
+                    color=op['trimcolor'][si],
+                    plot_type='trimcurve'
+                )
+
+        # Add bounding box
         vism.add(
-            ptsarr=ep['points'],
-            name=ep['name'],
-            color=(ep['color'], ep['size']),
-            plot_type='extras'
+            ptsarr=s.bbox,
+            name="bounding box" if ssz == 1 else "bounding box {}".format(si + 1),
+            color=op['bboxcolor'][si],
+            plot_type='bbox'
         )
 
-    # Process data requested by the visualization module
-    if vism.mconf['others']:
-        for vo in vism.mconf['others'].split(","):
-            vo_clean = vo.strip()
-            # Send center point of the parametric space to the visualization module
-            if vo_clean == "midpt":
-                vism.add(
-                    ptsarr=[spg.evaluate_single(*[r / 2.0 for r in spg.range()])],
-                    plot_type=vo_clean
-                )
+        # Add user plots
+        for ep in op['extras']:
+            vism.add(
+                ptsarr=ep['points'],
+                name=ep['name'],
+                color=(ep['color'], ep['size']),
+                plot_type='extras'
+            )
+
+        # Process data requested by the visualization module
+        if vism.mconf['others']:
+            for vo in vism.mconf['others'].split(","):
+                vo_clean = vo.strip()
+                # Send center point of the parametric space to the visualization module
+                if vo_clean == "midpt":
+                    vism.add(
+                        ptsarr=[s.evaluate_single(*[r / 2.0 for r in s.range()])],
+                        plot_type=vo_clean
+                    )
 
     # Display the figure
     if op['animate']:
