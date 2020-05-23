bd5950dcce05274344a3dc8960d14298ed24427c
Onur Rauf Bingol <orbingol@gmail.com>
2020-05-23 13:23:36 -0700

    Update construct module for 6.x and add tests

--- geomdl/construct.py
+++ geomdl/construct.py
@@ -20,7 +20,6 @@ def construct_surface(direction, *args, **kwargs):
     Keyword Arguments (optional):
         * ``degree``: degree of the 2nd parametric direction
         * ``knotvector``: knot vector of the 2nd parametric direction
-        * ``rational``: flag to generate rational surfaces
 
     :param direction: the direction that the input curves lies, i.e. u or v
     :type direction: str
@@ -34,12 +33,12 @@ def construct_surface(direction, *args, **kwargs):
 
     size_other = len(args)
     if size_other < 2:
-        raise GeomdlError("You need to input at least 2 curves")
+        raise GeomdlError("Input should be at least 2 curves")
 
     # Get keyword arguments
     degree_other = kwargs.get('degree', 2)
     knotvector_other = kwargs.get('knotvector', knotvector.generate(degree_other, size_other))
-    rational = kwargs.get('rational', args[0].rational)
+    rational = args[0].rational
 
     # Construct the control points of the new surface
     degree = args[0].degree.u
@@ -56,9 +55,9 @@ def construct_surface(direction, *args, **kwargs):
         new_ctrlpts += list(arg.ctrlpts)
         if rational:
             if not arg.weights:
-                raise GeomdlError("Expecting a rational curve",
-                                  data=dict(idx=idx, rational=rational, rational_arg=arg.rational))
-            new_weights += list(arg.weights)
+                new_weights += [1.0 for _ in range(arg.ctrlpts.count)]
+            else:
+                new_weights += list(arg.weights)
 
     # Set variables w.r.t. input direction
     if direction == 'v':
@@ -102,7 +101,6 @@ def construct_volume(direction, *args, **kwargs):
     Keyword Arguments (optional):
         * ``degree``: degree of the 3rd parametric direction
         * ``knotvector``: knot vector of the 3rd parametric direction
-        * ``rational``: flag to generate rational volumes
 
     :param direction: the direction that the input surfaces lies, i.e. u, v, w
     :type direction: str
@@ -116,84 +114,79 @@ def construct_volume(direction, *args, **kwargs):
 
     size_other = len(args)
     if size_other < 2:
-        raise GeomdlError("You need to input at least 2 surfaces")
+        raise GeomdlError("Input should be at least 2 surfaces")
 
     # Get keyword arguments
     degree_other = kwargs.get('degree', 1)
     knotvector_other = kwargs.get('knotvector', knotvector.generate(degree_other, size_other))
-    rational = kwargs.get('rational', args[0].rational)
+    rational = args[0].rational
 
     # Construct the control points of the new volume
-    degree_u, degree_v = args[0].degree_u, args[0].degree_v
-    size_u, size_v = args[0].ctrlpts_size_u, args[0].ctrlpts_size_v
+    degree_u, degree_v = args[0].degree.u, args[0].degree.v
+    size_u, size_v = args[0].ctrlpts_size.u, args[0].ctrlpts_size.v
     new_ctrlpts = []
     new_weights = []
     for idx, arg in enumerate(args):
-        if degree_u != arg.degree_u or degree_v != arg.degree_v:
+        if degree_u != arg.degree.u or degree_v != arg.degree.v:
             raise GeomdlError("Input surfaces must have the same degrees",
-                              data=dict(idx=idx, degree=(degree_u, degree_v), degree_arg=(arg.degree_u, arg.degree_v)))
-        if size_u != arg.ctrlpts_size_u or size_v != arg.ctrlpts_size_v:
+                              data=dict(idx=idx, degree=(degree_u, degree_v), degree_arg=(arg.degree.u, arg.degree.v)))
+        if size_u != arg.ctrlpts_size.u or size_v != arg.ctrlpts_size.v:
             raise GeomdlError("Input surfaces must have the same number of control points",
-                              data=dict(idx=idx, size=(size_u, size_v), size_arg=(arg.ctrlpts_size_u, arg.ctrlpts_size_v)))
+                              data=dict(idx=idx, size=(size_u, size_v), size_arg=(arg.ctrlpts_size.u, arg.ctrlpts_size.v)))
         new_ctrlpts += list(arg.ctrlpts)
         if rational:
-            if arg.weights is None:
-                raise GeomdlError("Expecting a rational surface",
-                                  data=dict(idx=idx, rational=rational, rational_arg=arg.rational))
-            new_weights += list(arg.weights)
+            if not arg.weights:
+                new_weights += [1.0 for _ in range(arg.ctrlpts.count)]
+            else:
+                new_weights += list(arg.weights)
 
     updated_ctrlpts = []
     updated_weights = []
 
     # Set variables w.r.t. input direction
     if direction == 'u':
-        degree_u, degree_v, degree_w = degree_other, args[0].degree_u, args[0].degree_v
-        size_u, size_v, size_w = size_other, args[0].ctrlpts_size_u, args[0].ctrlpts_size_v
-        kv_u, kv_v, kv_w = knotvector_other, args[0].knotvector_u, args[0].knotvector_v
+        degree_u, degree_v, degree_w = degree_other, args[0].degree.u, args[0].degree.v
+        size_u, size_v, size_w = size_other, args[0].ctrlpts_size.u, args[0].ctrlpts_size.v
+        kv_u, kv_v, kv_w = knotvector_other, args[0].knotvector.u, args[0].knotvector.v
         # u => w, v => u, w => v
         for v in range(0, size_v):
             for w in range(0, size_w):
                 for u in range(0, size_u):
-                    temp_pt = new_ctrlpts[v + (u * size_v) + (w * size_u * size_v)]
+                    temp_pt = new_ctrlpts[u + (v * size_u) + (w * size_u * size_v)]
                     updated_ctrlpts.append(temp_pt)
                     if rational:
-                        temp_w = new_weights[v + (u * size_v) + (w * size_u * size_v)]
+                        temp_w = new_weights[u + (v * size_u) + (w * size_u * size_v)]
                         updated_weights.append(temp_w)
     elif direction == 'v':
-        degree_u, degree_v, degree_w = args[0].degree_u, degree_other, args[0].degree_v
-        size_u, size_v, size_w = args[0].ctrlpts_size_u, size_other, args[0].ctrlpts_size_v
-        kv_u, kv_v, kv_w = args[0].knotvector_u, knotvector_other, args[0].knotvector_v
+        degree_u, degree_v, degree_w = args[0].degree.u, degree_other, args[0].degree.v
+        size_u, size_v, size_w = args[0].ctrlpts_size.u, size_other, args[0].ctrlpts_size.v
+        kv_u, kv_v, kv_w = args[0].knotvector.u, knotvector_other, args[0].knotvector.v
         # u => u, v => w, w => v
         for v in range(0, size_v):
             for u in range(0, size_u):
                 for w in range(0, size_w):
-                    temp_pt = new_ctrlpts[v + (u * size_v) + (w * size_u * size_v)]
+                    temp_pt = new_ctrlpts[u + (v * size_v) + (w * size_u * size_v)]
                     updated_ctrlpts.append(temp_pt)
                     if rational:
-                        temp_w = new_weights[v + (u * size_v) + (w * size_u * size_v)]
+                        temp_w = new_weights[u + (v * size_v) + (w * size_u * size_v)]
                         updated_weights.append(temp_w)
     else:  # direction == 'w'
-        degree_u, degree_v, degree_w = args[0].degree_u, args[0].degree_v, degree_other
-        size_u, size_v, size_w = args[0].ctrlpts_size_u, args[0].ctrlpts_size_v, size_other
-        kv_u, kv_v, kv_w = args[0].knotvector_u, args[0].knotvector_v, knotvector_other
+        degree_u, degree_v, degree_w = args[0].degree.u, args[0].degree.v, degree_other
+        size_u, size_v, size_w = args[0].ctrlpts_size.u, args[0].ctrlpts_size.v, size_other
+        kv_u, kv_v, kv_w = args[0].knotvector.u, args[0].knotvector.v, knotvector_other
         updated_ctrlpts = new_ctrlpts
         if rational:
             updated_weights = new_weights
 
     # Generate the volume
-    nv = shortcuts.generate_volume(rational)
-    nv.degree_u = degree_u
-    nv.degree_v = degree_v
-    nv.degree_w = degree_w
-    nv.ctrlpts_size_u = size_u
-    nv.ctrlpts_size_v = size_v
-    nv.ctrlpts_size_w = size_w
-    nv.ctrlpts = updated_ctrlpts
+    nv = NURBS.Volume()
+    nv.degree = (degree_u, degree_v, degree_w)
+    nv.knotvector = (kv_u, kv_v, kv_w)
+    cpm = control_points.CPManager(size_u, size_v, size_w)
+    cpm.points = new_ctrlpts
+    nv.ctrlpts = cpm
     if rational:
         nv.weights = updated_weights
-    nv.knotvector_u = kv_u
-    nv.knotvector_v = kv_v
-    nv.knotvector_w = kv_w
 
     return nv
 
@@ -227,7 +220,6 @@ def extract_curves(psurf, **kwargs):
 
     # Get data from the surface object
     surf_data = psurf.data
-    rational = surf_data['rational']
     degree_u = surf_data['degree'][0]
     degree_v = surf_data['degree'][1]
     kv_u = surf_data['knotvector'][0]
@@ -236,16 +228,13 @@ def extract_curves(psurf, **kwargs):
     size_v = surf_data['size'][1]
     cpts = surf_data['control_points']
 
-    # Determine object type
-    obj = shortcuts.generate_curve(rational)
-
     # v-direction
     crvlist_v = []
     if extract_v:
         for u in range(size_u):
-            curve = obj.__class__()
+            curve = NURBS.Curve()
             curve.degree = degree_v
-            curve.set_ctrlpts([cpts[v + (size_v * u)] for v in range(size_v)])
+            curve.set_ctrlpts([cpts[u + (size_u * v)] for v in range(size_v)])
             curve.knotvector = kv_v
             crvlist_v.append(curve)
 
@@ -253,9 +242,9 @@ def extract_curves(psurf, **kwargs):
     crvlist_u = []
     if extract_u:
         for v in range(size_v):
-            curve = obj.__class__()
+            curve = NURBS.Curve()
             curve.degree = degree_u
-            curve.set_ctrlpts([cpts[v + (size_v * u)] for u in range(size_u)])
+            curve.set_ctrlpts([cpts[u + (size_u * v)] for u in range(size_u)])
             curve.knotvector = kv_u
             crvlist_u.append(curve)
 
@@ -278,7 +267,6 @@ def extract_surfaces(pvol):
 
     # Get data from the volume object
     vol_data = pvol.data
-    rational = vol_data['rational']
     degree_u = vol_data['degree'][0]
     degree_v = vol_data['degree'][1]
     degree_w = vol_data['degree'][2]
@@ -290,18 +278,15 @@ def extract_surfaces(pvol):
     size_w = vol_data['size'][2]
     cpts = vol_data['control_points']
 
-    # Determine object type
-    obj = shortcuts.generate_surface(rational)
-
     # u-v plane
     surflist_uv = []
     for w in range(size_w):
-        surf = obj.__class__()
+        surf = NURBS.Surface()
         surf.degree_u = degree_u
         surf.degree_v = degree_v
         surf.ctrlpts_size_u = size_u
         surf.ctrlpts_size_v = size_v
-        surf.ctrlpts2d = [[cpts[v + (size_v * (u + (size_u * w)))] for v in range(size_v)] for u in range(size_u)]
+        surf.ctrlpts2d = [[cpts[u + (size_u * (v + (size_v * w)))] for v in range(size_v)] for u in range(size_u)]
         surf.knotvector_u = kv_u
         surf.knotvector_v = kv_v
         surflist_uv.append(surf)
@@ -309,12 +294,12 @@ def extract_surfaces(pvol):
     # u-w plane
     surflist_uw = []
     for v in range(size_v):
-        surf = obj.__class__()
+        surf = NURBS.Surface()
         surf.degree_u = degree_u
         surf.degree_v = degree_w
         surf.ctrlpts_size_u = size_u
         surf.ctrlpts_size_v = size_w
-        surf.ctrlpts2d = [[cpts[v + (size_v * (u + (size_u * w)))] for w in range(size_w)] for u in range(size_u)]
+        surf.ctrlpts2d = [[cpts[u + (size_u * (v + (size_v * w)))] for w in range(size_w)] for u in range(size_u)]
         surf.knotvector_u = kv_u
         surf.knotvector_v = kv_w
         surflist_uw.append(surf)
@@ -322,12 +307,12 @@ def extract_surfaces(pvol):
     # v-w plane
     surflist_vw = []
     for u in range(size_u):
-        surf = obj.__class__()
+        surf = NURBS.Surface()
         surf.degree_u = degree_v
         surf.degree_v = degree_w
         surf.ctrlpts_size_u = size_v
         surf.ctrlpts_size_v = size_w
-        surf.ctrlpts2d = [[cpts[v + (size_v * (u + (size_u * w)))] for w in range(size_w)] for v in range(size_v)]
+        surf.ctrlpts2d = [[cpts[u + (size_u * (v + (size_v * w)))] for w in range(size_w)] for v in range(size_v)]
         surf.knotvector_u = kv_v
         surf.knotvector_v = kv_w
         surflist_vw.append(surf)
--- tests/test_construct.py
+++ tests/test_construct.py
@@ -6,9 +6,11 @@
     Requires "pytest" to run.
 """
 
+import pytest
 from pytest import fixture
 from geomdl import NURBS
 from geomdl import construct
+from geomdl.base import GeomdlError
 
 
 @fixture
@@ -27,6 +29,72 @@ def curve2():
     c.knotvector = [0, 0, 0, 0, 1, 2, 2, 2, 2]
     c.set_ctrlpts([[1.0, 1.0, 1.0, 1.0], [2.0, 3.0, 1.0, 1.0], [5.0, 3.0, 2.0, 1.0], [5.0, 4.0, 1.5, 1.0], [6.0, 6.0, 1.0, 1.0]])
     return c
+
+
+@fixture
+def curve3():
+    c = NURBS.Curve()
+    c.degree = 3
+    c.knotvector = [0, 0, 0, 0, 1, 2, 2, 2, 2]
+    c.set_ctrlpts([[1.0, 1.0, 4.0, 1.0], [2.0, 3.0, 4.0, 1.0], [5.0, 3.0, 4.0, 1.0], [5.0, 4.0, 4.0, 1.0], [6.0, 6.0, 4.0, 1.0]])
+    return c
+
+
+@fixture
+def curve4():
+    c = NURBS.Curve()
+    c.degree = 3
+    c.knotvector = [0, 0, 0, 0, 1, 2, 2, 2, 2]
+    c.set_ctrlpts([[2.0, 1.0, 0.0, 1.0], [3.0, 3.0, 0.0, 1.0], [6.0, 3.0, 1.0, 1.0], [6.0, 4.0, 0.5, 1.0], [7.0, 6.0, 0.0, 1.0]])
+    return c
+
+
+@fixture
+def curve5():
+    c = NURBS.Curve()
+    c.degree = 3
+    c.knotvector = [0, 0, 0, 0, 1, 2, 2, 2, 2]
+    c.set_ctrlpts([[2.0, 1.0, 1.0, 1.0], [3.0, 3.0, 1.0, 1.0], [6.0, 3.0, 2.0, 1.0], [6.0, 4.0, 1.5, 1.0], [7.0, 6.0, 1.0, 1.0]])
+    return c
+
+
+@fixture
+def curve6():
+    c = NURBS.Curve()
+    c.degree = 3
+    c.knotvector = [0, 0, 0, 0, 1, 2, 2, 2, 2]
+    c.set_ctrlpts([[2.0, 1.0, 4.0, 1.0], [3.0, 3.0, 4.0, 1.0], [6.0, 3.0, 4.0, 1.0], [6.0, 4.0, 4.0, 1.0], [7.0, 6.0, 4.0, 1.0]])
+    return c
+
+
+@fixture
+def surface1(curve1, curve2, curve3):
+    s = construct.construct_surface("u", curve1, curve2, curve3)
+    return s
+
+
+@fixture
+def surface2(curve4, curve5, curve6):
+    s = construct.construct_surface("u", curve4, curve5, curve6)
+    return s
+
+
+@fixture
+def surface3(curve1, curve3):
+    s = construct.construct_surface("v", curve1, curve3)
+    return s
+
+
+@fixture
+def surface4(curve4, curve6):
+    s = construct.construct_surface("v", curve4, curve6)
+    return s
+
+
+@fixture
+def volume1(surface1, surface2):
+    v = construct.construct_volume("w", surface1, surface2)
+    return v
 
 
 def test_construct_surface_v_degree(curve1, curve2):
@@ -63,3 +131,67 @@ def test_construct_surface_u_ctrlpts(curve1, curve2):
     surf = construct.construct_surface('u', curve1, curve2)
     assert all([x == y for x, y in zip(surf.ctrlpts[1], curve2.ctrlpts[0])])
     assert all([x == y for x, y in zip(surf.ctrlpts[2], curve1.ctrlpts[1])])
+
+
+def test_construct_volume_u_degree(surface1, surface2):
+    vol = construct.construct_volume('u', surface1, surface2)
+    assert vol.degree.u == 1
+    assert vol.degree.v == surface1.degree.u
+    assert vol.degree.v == surface2.degree.u
+    assert vol.degree.w == surface1.degree.v
+    assert vol.degree.w == surface2.degree.v
+
+
+def test_construct_volume_v_degree(surface1, surface2):
+    vol = construct.construct_volume('v', surface1, surface2)
+    assert vol.degree.u == surface1.degree.u
+    assert vol.degree.u == surface2.degree.u
+    assert vol.degree.v == 1
+    assert vol.degree.w == surface1.degree.v
+    assert vol.degree.w == surface2.degree.v
+
+
+def test_construct_volume_w_degree(surface1, surface2):
+    vol = construct.construct_volume('w', surface1, surface2)
+    assert vol.degree.u == surface1.degree.u
+    assert vol.degree.u == surface2.degree.u
+    assert vol.degree.v == surface1.degree.v
+    assert vol.degree.v == surface2.degree.v
+    assert vol.degree.w == 1
+
+
+def test_extract_curves_error1(volume1):
+    with pytest.raises(GeomdlError):
+        construct.extract_curves(volume1)
+
+def test_extract_curves1(surface1):
+    cs = construct.extract_curves(surface1)
+    assert isinstance(cs, dict)
+    assert "u" in cs.keys()
+    assert "v" in cs.keys()
+
+
+def test_extract_curves2(surface1):
+    cs = construct.extract_curves(surface1)
+    assert len(cs['u']) == surface1.ctrlpts_size.v
+    assert len(cs['v']) == surface1.ctrlpts_size.u
+
+
+def test_extract_surfaces_error1(surface1):
+    with pytest.raises(GeomdlError):
+        construct.extract_surfaces(surface1)
+
+
+def test_extract_surfaces1(volume1):
+    ss = construct.extract_surfaces(volume1)
+    assert isinstance(ss, dict)
+    assert "uv" in ss.keys()
+    assert "uw" in ss.keys()
+    assert "vw" in ss.keys()
+
+
+def test_extract_surfaces2(volume1):
+    ss = construct.extract_surfaces(volume1)
+    assert len(ss['uv']) == volume1.ctrlpts_size.w
+    assert len(ss['uw']) == volume1.ctrlpts_size.v
+    assert len(ss['vw']) == volume1.ctrlpts_size.u
