d880e7018e50684dd37a49cebaaf3546c5464e32
Onur Rauf Bingol <orbingol@gmail.com>
2020-05-25 19:02:25 -0700

    Add file header to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
+"""
+    pytest configuration for the NURBS-Python (geomdl) package
+    Released under The MIT License. See LICENSE file for details.
+    Copyright (c) 2020 Onur Rauf Bingol
+"""
+
 import pytest
 from tests.fixtures.curves import *
 from tests.fixtures.surfaces import surface1, surface2, surface3, surface4
