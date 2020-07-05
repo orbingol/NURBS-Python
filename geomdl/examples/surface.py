92b01edc7e27e38b3d2045e879b9ed0206b9828e
Onur Rauf Bingol <orbingol@gmail.com>
2020-07-04 19:41:10 -0700

    Add a new surface example

--- geomdl/examples/surface.py
+++ geomdl/examples/surface.py
@@ -8,7 +8,7 @@
 """
 
 from .. import base
-from .. import NURBS
+from .. import BSpline, NURBS
 from ..ptmanager import CPManager
 
 
@@ -51,8 +51,34 @@ def cylinder(radius=1.0, height=1.0):
     # Return the generated surface
     return surface
 
-
 def surface_ex1():
+    """ Creates an example B-Spline surface
+
+    degree_u=3, degree_v=3, size_u=6, size_v=6
+
+    :return: surface
+    :rtype: NURBS.Surface
+    """
+    cpts = [
+        [-25,-25,-10], [-25,-15,-5] , [25,-5,0], [-25,5,0], [-25,15,-5], [-25,25,-10],
+        [-15,-25,-8], [-15,-15,-4], [-15,-5,-4], [-15,5,-4], [-15,15,-4], [-15,25,-8],
+        [-5,-25,-5], [-5,-15,-3], [-5,-5,-8], [-5,5,-8], [-5,15,-3], [-5,25,-5],
+        [5,-25,-3], [5,-15,-2], [5,-5,-8], [5,5,-8], [5,15,-2], [5,25,-3],
+        [15,-25,-8], [15,-15,-4], [15,-5,-4], [15,5,-4], [15,15,-4], [15,25,-8],
+        [25,-25,-10], [25,-15,-5], [25,-5,2], [25,5,2], [25,15,-5], [25,25,-10]
+    ]
+
+    # Create a BSpline surface instance
+    surf = BSpline.Surface()
+    surf.degree.u = 3
+    surf.degree.v = 3
+    surf.knotvector.u = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
+    surf.knotvector.v = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
+    surf.set_ctrlpts(cpts, 6, 6)
+
+    return surf
+
+
 def surface_ex2():
     """ Creates an example NURBS surface
 
@@ -63,7 +89,6 @@ def surface_ex2():
     :return: surface
     :rtype: NURBS.Surface
     """
-
     # Create control points manager
     cpman = CPManager(6, 6)
     cpman.points = [
