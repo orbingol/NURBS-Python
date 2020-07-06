44c554baf4e961b48f65cc5bdd9ec10c1d75f89c
Onur Rauf Bingol <orbingol@gmail.com>
2020-07-06 02:48:35 -0700

    Add VisPlotly tests

---
+++ tests/visualization/test_visplotly.py
@@ -0,0 +1,169 @@
+"""
+    Tests for the NURBS-Python (geomdl) package
+    Released under The MIT License. See LICENSE file for details.
+    Copyright (c) 2020 Onur Rauf Bingol
+
+    Tests VisPlotly visualization module. Requires "pytest" to run.
+"""
+
+import os
+import pytest
+from geomdl import BSpline
+from geomdl.visualization import VisPlotly as vis
+from geomdl.visualization import render
+
+
+@pytest.mark.usefixtures("curve7")
+def test_curve2d_fig_nowindow(curve7):
+    conf = vis.VisConfig()
+    vism = vis.VisCurve2D(config=conf)
+
+    fname = conf.figure_image_filename
+
+    render.render(curve7, vism)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(conf.figure_image_filename):
+        os.remove(conf.figure_image_filename)
+
+
+@pytest.mark.usefixtures("curve8")
+def test_curve2d_fig_save(curve8):
+    conf = vis.VisConfig()
+    vism = vis.VisCurve2D(config=conf)
+
+    fname = "test-curve.png"
+
+    render.render(curve8, vism, filename=fname, display=False)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(fname):
+        os.remove(fname)
+
+
+@pytest.mark.usefixtures("curve6")
+def test_curve3d_fig_nowindow(curve6):
+    conf = vis.VisConfig()
+    vism = vis.VisCurve3D(config=conf)
+
+    fname = conf.figure_image_filename
+
+    render.render(curve6, vism)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(conf.figure_image_filename):
+        os.remove(conf.figure_image_filename)
+
+
+@pytest.mark.usefixtures("curve6")
+def test_curve3d_fig_save(curve6):
+    conf = vis.VisConfig()
+    vism = vis.VisCurve3D(config=conf)
+
+    fname = "test-curve.png"
+
+    render.render(curve6, vism, filename=fname, display=False)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(fname):
+        os.remove(fname)
+
+
+@pytest.mark.usefixtures("surface1")
+def test_surf_fig_nowindow(surface1):
+    conf = vis.VisConfig()
+    vism = vis.VisSurface(config=conf)
+
+    fname = conf.figure_image_filename
+
+    render.render(surface1, vism)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(conf.figure_image_filename):
+        os.remove(conf.figure_image_filename)
+
+
+@pytest.mark.usefixtures("surface1")
+def test_surf_fig_save(surface1):
+    conf = vis.VisConfig()
+    vism = vis.VisSurface(config=conf)
+
+    fname = "test-surface.png"
+
+    render.render(surface1, vism, filename=fname, display=False)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(fname):
+        os.remove(fname)
+
+
+@pytest.mark.usefixtures("surface1")
+def test_surf_ctrlpts_offset(surface1):
+    conf = vis.VisConfig()
+    vism = vis.VisSurface(config=conf)
+
+    # Set control points grid offset
+    vis.ctrlpts_offset = 3.5
+
+    fname = "test-surface.png"
+
+    render.render(surface1, vism, filename=fname, display=False)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(fname):
+        os.remove(fname)
+
+
+@pytest.mark.usefixtures("volume1")
+def test_vol_fig_nowindow(volume1):
+    conf = vis.VisConfig()
+    vism = vis.VisVolume(config=conf)
+
+    fname = conf.figure_image_filename
+
+    render.render(volume1, vism)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(conf.figure_image_filename):
+        os.remove(conf.figure_image_filename)
+
+
+@pytest.mark.usefixtures("volume1")
+def test_vol_fig_save(volume1):
+    conf = vis.VisConfig()
+    vism = vis.VisVolume(config=conf)
+
+    fname = "test-surface.png"
+
+    render.render(volume1, vism, filename=fname, display=False)
+
+    assert os.path.isfile(fname)
+    assert os.path.getsize(fname) > 0
+
+    # Clean up temporary file if exists
+    if os.path.isfile(fname):
+        os.remove(fname)
