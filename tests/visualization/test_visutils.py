46dc8efd0116e15c48dc9e0744a79ddc6488df03
Onur Rauf Bingol <orbingol@gmail.com>
2020-07-06 03:15:44 -0700

    Fix function arguments

--- tests/visualization/test_visutils.py
+++ tests/visualization/test_visutils.py
@@ -10,7 +10,9 @@ from geomdl.visualization import visutils
 
 
 def test_color_generator():
+    num = 4
     seed = 17  # some number to be used as the random seed
-    result = visutils.color_generator(seed)
-    to_check = visutils.color_generator(seed)
+    result = visutils.color_generator(num=num, seed=seed)
+    to_check = visutils.color_generator(num=num, seed=seed)
     assert to_check == result
+    assert len(to_check) == len(result)
